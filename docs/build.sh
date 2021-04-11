16ca8c8086fc20ba5a9a10f81aea249a9cc7616f
Charith Ellawala <charith@cerbos.dev>
2021-04-11 10:50:10 +0100

    chore: Make docs build script work on MacOS

--- docs/build.sh
+++ docs/build.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SOURCE_DIR=$(readlink -m "${SCRIPT_DIR}/..")
+SOURCE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 WORKSPACE="/github/workspace"
-#OUT_DIR=$(readlink -m "${SCRIPT_DIR}/../../dist")
 
-#mkdir -p "$OUT_DIR"
-#docker run -v "$SCRIPT_DIR":/antora:Z -v "$SOURCE_DIR":/cerbos:Z -v "$OUT_DIR":/out:Z --rm -t antora/antora antora-playbook.yml
 docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:latest antora --stacktrace "${WORKSPACE}/docs/antora-playbook.yml"
