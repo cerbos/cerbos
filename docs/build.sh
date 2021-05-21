e9d81fb62d81655756fa46cbad6fe0208181da5a
Charith Ellawala <charith@cerbos.dev>
2021-05-21 15:49:55 +0100

    ci: Update docs build configuration
    Fix hardcoded GitHub actions checkout paths

--- docs/build.sh
+++ docs/build.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 WORKSPACE="/github/workspace"
 
-docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:latest antora --stacktrace "${WORKSPACE}/docs/antora-playbook.yml"
+docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:latest antora --stacktrace "${WORKSPACE}/docs/dev-antora-playbook.yml"
 
 UNAME=$(uname -s)
 OPEN_CMD=xdg-open
