4ff1e9caee6dfe26594a79d1f83fb0d21537d8d6
Charith Ellawala <charithe@users.noreply.github.com>
2021-06-22 14:36:39 +0100

    chore(perf): Use VT proto plugin (#173)
    Use the vtprotobuf plugin to generate more optimized serde code for the
    protobufs. This change improves the throughput of the gRPC service.

---
+++ internal/server/codec.go
@@ -0,0 +1,54 @@
+// Copyright 2021 Zenauth Ltd.
+
+package server
+
+import (
+	"fmt"
+
+	vtgrpc "github.com/planetscale/vtprotobuf/codec/grpc"
+	"google.golang.org/grpc/encoding"
+
+	// Import the default grpc encoding to ensure that it gets replaced by this codec.
+	_ "google.golang.org/grpc/encoding/proto"
+	"google.golang.org/protobuf/proto"
+)
+
+const name = "proto"
+
+func init() {
+	// Register the codec to use VT where possible for optimized marshaling/unmarshaling.
+	encoding.RegisterCodec(Codec{vtcodec: vtgrpc.Codec{}})
+}
+
+// Codec implements the grpc Codec interface to delegate encoding to VT where possible.
+type Codec struct {
+	vtcodec vtgrpc.Codec
+}
+
+func (c Codec) Name() string {
+	return name
+}
+
+func (c Codec) Marshal(v interface{}) ([]byte, error) {
+	if b, err := c.vtcodec.Marshal(v); err == nil {
+		return b, nil
+	}
+
+	vv, ok := v.(proto.Message)
+	if !ok {
+		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
+	}
+	return proto.Marshal(vv)
+}
+
+func (c Codec) Unmarshal(data []byte, v interface{}) error {
+	if err := c.vtcodec.Unmarshal(data, v); err == nil {
+		return nil
+	}
+
+	vv, ok := v.(proto.Message)
+	if !ok {
+		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
+	}
+	return proto.Unmarshal(data, vv)
+}
