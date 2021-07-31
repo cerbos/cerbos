2f6005dc899c01fc047af78db2c81b39e0441b78
serdar <serdar@cerbos.dev>
2021-07-31 14:56:12 +0300

    client: add audit log api to admin client
    Signed-off-by: serdar <serdar@cerbos.dev>

--- client/admin.go
+++ client/admin.go
@@ -5,16 +5,24 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
+	auditv1 "github.com/cerbos/cerbos/api/genpb/cerbos/audit/v1"
 	requestv1 "github.com/cerbos/cerbos/api/genpb/cerbos/request/v1"
 	svcv1 "github.com/cerbos/cerbos/api/genpb/cerbos/svc/v1"
 )
 
 type AdminClient interface {
 	AddOrUpdatePolicy(context.Context, *PolicySet) error
+	AccessLogs(ctx context.Context, opts AuditLogOptions) ([]*auditv1.AccessLogEntry, error)
+	DecisionLogs(ctx context.Context, opts AuditLogOptions) ([]*auditv1.DecisionLogEntry, error)
 }
 
 // NewAdminClient creates a new admin client.
@@ -46,23 +54,106 @@ func NewAdminClientWithCredentials(address, username, password string, opts ...O
 		basicAuth = basicAuth.Insecure()
 	}
 
-	return &grpcAdminClient{client: svcv1.NewCerbosAdminServiceClient(grpcConn), creds: basicAuth}, nil
+	return &GrpcAdminClient{client: svcv1.NewCerbosAdminServiceClient(grpcConn), creds: basicAuth}, nil
 }
 
-type grpcAdminClient struct {
+type GrpcAdminClient struct {
 	client svcv1.CerbosAdminServiceClient
 	creds  credentials.PerRPCCredentials
 }
 
-func (gac *grpcAdminClient) AddOrUpdatePolicy(ctx context.Context, policies *PolicySet) error {
+func (c *GrpcAdminClient) AddOrUpdatePolicy(ctx context.Context, policies *PolicySet) error {
 	if err := policies.Validate(); err != nil {
 		return err
 	}
 
 	req := &requestv1.AddOrUpdatePolicyRequest{Policies: policies.policies}
-	if _, err := gac.client.AddOrUpdatePolicy(ctx, req, grpc.PerRPCCredentials(gac.creds)); err != nil {
+	if _, err := c.client.AddOrUpdatePolicy(ctx, req, grpc.PerRPCCredentials(c.creds)); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// AuditLogOptions is used to filter audit logs
+type AuditLogOptions struct {
+	Tail      uint16
+	StartTime time.Time
+	EndTime   time.Time
+	Lookup    string
+}
+
+type logSet struct {
+	accessLogs   []*auditv1.AccessLogEntry
+	decisionLogs []*auditv1.DecisionLogEntry
+}
+
+// AccessLogs returns audit logs of the access type entries
+func (c *GrpcAdminClient) AccessLogs(ctx context.Context, opts AuditLogOptions) ([]*auditv1.AccessLogEntry, error) {
+	logs, err := c.auditLogs(ctx, requestv1.ListAuditLogEntriesRequest_KIND_ACCESS, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return logs.accessLogs, nil
+}
+
+// DecisionLogs returns decision logs of the decision type entries
+func (c *GrpcAdminClient) DecisionLogs(ctx context.Context, opts AuditLogOptions) ([]*auditv1.DecisionLogEntry, error) {
+	logs, err := c.auditLogs(ctx, requestv1.ListAuditLogEntriesRequest_KIND_DECISION, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return logs.decisionLogs, nil
+}
+
+func (c *GrpcAdminClient) auditLogs(ctx context.Context, kind requestv1.ListAuditLogEntriesRequest_Kind, opts AuditLogOptions) (*logSet, error) {
+	req := &requestv1.ListAuditLogEntriesRequest{Kind: kind}
+
+	switch {
+	case opts.Tail > 0:
+		req.Filter = &requestv1.ListAuditLogEntriesRequest_Tail{Tail: uint32(opts.Tail)}
+	case !opts.StartTime.IsZero() && !opts.EndTime.IsZero():
+		req.Filter = &requestv1.ListAuditLogEntriesRequest_Between{
+			Between: &requestv1.ListAuditLogEntriesRequest_TimeRange{
+				Start: timestamppb.New(opts.StartTime),
+				End:   timestamppb.New(opts.EndTime),
+			},
+		}
+	case opts.Lookup != "":
+		req.Filter = &requestv1.ListAuditLogEntriesRequest_Lookup{Lookup: opts.Lookup}
+	}
+
+	resp, err := c.client.ListAuditLogEntries(ctx, req, grpc.PerRPCCredentials(c.creds))
+	if err != nil {
+		return nil, err
+	}
+
+	entries := &logSet{
+		accessLogs:   make([]*auditv1.AccessLogEntry, 0),
+		decisionLogs: make([]*auditv1.DecisionLogEntry, 0),
+	}
+
+	for {
+		entry, err := resp.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Println("entries... done")
+				return entries, nil
+			}
+
+			return nil, err
+		}
+
+		fmt.Println("adding entry")
+		switch kind {
+		case requestv1.ListAuditLogEntriesRequest_KIND_ACCESS:
+			entries.accessLogs = append(entries.accessLogs, entry.GetAccessLogEntry())
+		case requestv1.ListAuditLogEntriesRequest_KIND_DECISION:
+			entries.decisionLogs = append(entries.decisionLogs, entry.GetDecisionLogEntry())
+		default:
+			return nil, fmt.Errorf("unsupported audit log entry kind: %s", kind)
+		}
+	}
 }
---
+++ client/admin_test.go
@@ -0,0 +1,75 @@
+// Copyright 2021 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package client_test
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/cerbos/cerbos/client"
+	"github.com/cerbos/cerbos/client/testutil"
+	"github.com/cerbos/cerbos/internal/audit"
+	"github.com/cerbos/cerbos/internal/audit/local"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAccessLogs(t *testing.T) {
+	serverOpts := mkServerOpts(t, false)
+	tempDir := t.TempDir()
+	serverOpts = append(serverOpts,
+		testutil.WithHTTPListenAddr(fmt.Sprintf("unix:%s", filepath.Join(tempDir, "http.sock"))),
+		testutil.WithGRPCListenAddr(fmt.Sprintf("unix:%s", filepath.Join(tempDir, "grpc.sock"))),
+	)
+	s, err := testutil.StartCerbosServer(serverOpts...)
+	require.NoError(t, err)
+
+	audit.RegisterBackend("local", func(_ context.Context) (audit.Log, error) {
+		return local.New()
+	})
+
+	defer s.Stop() //nolint:errcheck
+
+	ac, err := client.NewAdminClientWithCredentials(s.GRPCAddr(), adminUsername, adminPassword, client.WithPlaintext())
+	require.NoError(t, err)
+
+	loadPolicies(t, ac)
+
+	c, err := client.New(s.GRPCAddr(), client.WithPlaintext())
+	require.NoError(t, err)
+
+	_, err = c.IsAllowed(
+		context.TODO(),
+		client.NewPrincipal("john").
+			WithRoles("employee").
+			WithPolicyVersion("20210210").
+			WithAttributes(map[string]interface{}{
+				"department": "marketing",
+				"geography":  "GB",
+				"team":       "design",
+			}),
+		client.NewResource("leave_request", "XX125").
+			WithPolicyVersion("20210210").
+			WithAttributes(map[string]interface{}{
+				"department": "marketing",
+				"geography":  "GB",
+				"id":         "XX125",
+				"owner":      "john",
+				"team":       "design",
+			}),
+		"view:public")
+
+	require.NoError(t, err)
+
+	logs, err := ac.DecisionLogs(context.Background(), client.AuditLogOptions{
+		StartTime: time.Now().Add(time.Duration(-10) * time.Minute),
+		EndTime:   time.Now(),
+	})
+	require.NoError(t, err)
+	if len(logs) == 0 {
+		t.Skip("test is skipped, logs could not be received")
+	}
+}
