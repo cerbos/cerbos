7c68e1e2892b118f4f3c1f264851210ab799fa67
Charith Ellawala <charith@cerbos.dev>
2021-08-23 18:29:03 +0100

    chore: Configure versioned docs
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- docs/build.sh
+++ docs/build.sh
@@ -7,8 +7,9 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 WORKSPACE="/github/workspace"
+ANTORA_VERSION=${ANTORA_VERSION:-"latest"}
 
-docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:latest antora --stacktrace "${WORKSPACE}/docs/antora-playbook.yml"
+docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:${ANTORA_VERSION} antora --stacktrace --clean "${WORKSPACE}/docs/antora-playbook.yml"
 
 VERSION=$(awk '/^version:/ {print $2}' "${SCRIPT_DIR}/antora.yml" | tr -d '"')
 
