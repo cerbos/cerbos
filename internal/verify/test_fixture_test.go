2cccd69ad254d895f1fd50d51fc50f4e554b6a40
Dennis Buduev <dennis@cerbos.dev>
2021-09-15 21:07:08 +1200

    Test_testFixture_getTests
    Signed-off-by: Dennis Buduev <dennis@cerbos.dev>

--- internal/verify/test_fixture_test.go
+++ internal/verify/test_fixture_test.go
@@ -1,7 +1,9 @@
 package verify
 
 import (
+	effectv1 "github.com/cerbos/cerbos/api/genpb/cerbos/effect/v1"
 	v1 "github.com/cerbos/cerbos/api/genpb/cerbos/engine/v1"
+	policyv1 "github.com/cerbos/cerbos/api/genpb/cerbos/policy/v1"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/testing/protocmp"
@@ -59,5 +61,76 @@ func Test_loadPrincipals(t *testing.T) {
 				t.Errorf("loadPrincipals() diff = %s", diff)
 			}
 		})
+	}
+}
+
+func Test_testFixture_getTests(t *testing.T) {
+	harry := &v1.Principal{Id: "harry"}
+	maggie := &v1.Principal{Id: "maggie"}
+	harryLeaveRequest := &v1.Resource{Id: "harry_leave_request"}
+
+	tf := &testFixture{
+		principals: map[string]*v1.Principal{
+			"harry":  harry,
+			"maggie": maggie,
+		},
+		resources: map[string]*v1.Resource{
+			"harry_leave_request": harryLeaveRequest,
+		},
+	}
+	requestId := "requestId"
+	name := "harry leave request"
+	actions := []string{"view", "approve"}
+	table := &policyv1.TestTable{
+		Name: name,
+		Input: &policyv1.TestTable_CheckInput{
+			RequestId: requestId,
+			Resource:  harryLeaveRequest.Id,
+			Actions:   actions,
+		},
+		Expected: []*policyv1.TestTable_ExpectedItem{
+			{
+				Principal: "harry",
+				Actions: map[string]effectv1.Effect{
+					"view":    effectv1.Effect_EFFECT_ALLOW,
+					"approve": effectv1.Effect_EFFECT_DENY,
+				},
+			},
+			{
+				Principal: "maggie",
+				Actions: map[string]effectv1.Effect{
+					"view":    effectv1.Effect_EFFECT_ALLOW,
+					"approve": effectv1.Effect_EFFECT_ALLOW,
+				},
+			},
+		},
+	}
+	ts := &policyv1.TestSuite{Tests: []*policyv1.TestTable{table}}
+	expectedTests := []*policyv1.Test{{
+		Name: name,
+		Input: &v1.CheckInput{
+			RequestId: requestId,
+			Resource:  harryLeaveRequest,
+			Principal: harry,
+			Actions:   actions,
+		},
+		Expected: table.Expected[0].Actions,
+	},
+	{
+		Name: name,
+		Input: &v1.CheckInput{
+			RequestId: requestId,
+			Resource:  harryLeaveRequest,
+			Principal: maggie,
+			Actions:   actions,
+		},
+		Expected: table.Expected[1].Actions,
+	},
+	}
+	got, err := tf.getTests(ts)
+	require.NoError(t, err)
+
+	if diff := cmp.Diff(got, expectedTests, protocmp.Transform()); diff != "" {
+		t.Errorf("getTests: diff = %s", diff)
 	}
 }
