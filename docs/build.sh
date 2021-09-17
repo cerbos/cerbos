633f1b39af04ae43677a6526e70695db8e854aad
Charith Ellawala <charithe@users.noreply.github.com>
2021-09-17 15:11:44 +0100

    docs: Preserve older versions (#296)
    * chore: Configure versioned docs
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Configure multiple outputs
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Update build scripts
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Update version in GH Action
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- docs/build.sh
+++ docs/build.sh
@@ -7,8 +7,10 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_DIR=$(cd "${SCRIPT_DIR}/.." && pwd)
 WORKSPACE="/github/workspace"
+ANTORA_VERSION=${ANTORA_VERSION:-"3.0.0-alpha.9"}
 
-docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:latest antora --stacktrace "${WORKSPACE}/docs/antora-playbook.yml"
+rm -rf ${SCRIPT_DIR}/build
+docker run -v "$SOURCE_DIR":"$WORKSPACE":Z --rm -t docker.io/antora/antora:${ANTORA_VERSION} antora --stacktrace --clean "${WORKSPACE}/docs/antora-playbook.yml"
 
 VERSION=$(awk '/^version:/ {print $2}' "${SCRIPT_DIR}/antora.yml" | tr -d '"')
 
