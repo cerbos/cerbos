3a44495e6e431c391a07a1d3d04cd2156b969979
Charith Ellawala <charithe@users.noreply.github.com>
2021-10-15 10:44:44 +0100

    chore(ci): Update goreleaser configuration (#359)
    - Update tools to use latest version of goreleaser
    - Configure macOS universal binary builds
    - Change version reporting in binaries
    - Replace deprecated settings with new alternatives
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- internal/util/app.go
+++ internal/util/app.go
@@ -3,9 +3,27 @@
 
 package util
 
+import (
+	"fmt"
+	"runtime/debug"
+	"strings"
+)
+
 var (
 	AppName   = "cerbos"
 	BuildDate = "unknown"
 	Commit    = "unknown"
 	Version   = "unknown"
 )
+
+func AppVersion() string {
+	var sb strings.Builder
+	_, _ = sb.WriteString(Version)
+	_, _ = sb.WriteString(fmt.Sprintf("\nBuilt on %s from %s\n", BuildDate, Commit))
+
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Sum != "" {
+		_, _ = sb.WriteString(fmt.Sprintf("Module version: %s, Module checksum: %s\n", info.Main.Version, info.Main.Sum))
+	}
+
+	return sb.String()
+}
