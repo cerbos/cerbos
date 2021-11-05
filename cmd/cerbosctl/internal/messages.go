2a1c2ff183c76cf8b62cc5eef34893bbafd9237c
isdevx <87911166+isdevx@users.noreply.github.com>
2021-11-05 18:36:39 +0300

    enhancement: Provide issue submission link for panics at cerbosctl (#283)
    * provide issue submission link for errors
    
    Signed-off-by: serdar <serdar@cerbos.dev>
    
    * add nolint
    
    Signed-off-by: serdar <serdar@cerbos.dev>
    
    * Update cmd/cerbosctl/internal/messages.go
    
    Co-authored-by: Charith Ellawala <charithe@users.noreply.github.com>
    Signed-off-by: serdar <serdar@cerbos.dev>
    
    * reflect review comments
    
    Signed-off-by: Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
    
    Co-authored-by: Charith Ellawala <charithe@users.noreply.github.com>
    Co-authored-by: Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>

---
+++ cmd/cerbosctl/internal/messages.go
@@ -0,0 +1,38 @@
+// Copyright 2021 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package internal
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const (
+	FeedbackMsg  = "It looks like an unexpected error happened. Please use the link below to report it to Cerbos developers."
+	baseURL      = "https://github.com/cerbos/cerbos/issues/new"
+	FeedbackLink = baseURL + "/choose"
+)
+
+func GenerateFeedbackLink(header, version, commitSHA string, stack []byte) string {
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		panic(err) // this should never happen
+	}
+	q := u.Query()
+	q.Set("labels", "bug")
+	q.Set("title", header)
+	var body string
+	if version != "" {
+		body += fmt.Sprintf("Version: %s\n", version)
+	}
+	if commitSHA != "" {
+		body += fmt.Sprintf("Commit: %s\n", commitSHA)
+	}
+	if stack != nil {
+		body += fmt.Sprintf("\nStack:\n```\n%s\n```\n", stack)
+	}
+	q.Set("body", body)
+	u.RawQuery = q.Encode()
+	return fmt.Sprint(u)
+}
--- cmd/cerbosctl/main.go
+++ cmd/cerbosctl/main.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"runtime/debug"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -14,6 +15,7 @@ import (
 	"github.com/cerbos/cerbos/client"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/audit"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/decisions"
+	"github.com/cerbos/cerbos/cmd/cerbosctl/internal"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/list"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/version"
 	"github.com/cerbos/cerbos/internal/util"
@@ -79,9 +81,19 @@ func main() {
 
 	cmd.AddCommand(audit.NewAuditCmd(withAdminClient), decisions.NewDecisionsCmd(withAdminClient), version.NewVersionCmd(withClient), list.NewListCmd(withAdminClient))
 
+	defer func() {
+		if x := recover(); x != nil {
+			cmd.PrintErrln(internal.FeedbackMsg + "\n")
+			cmd.PrintErrln(internal.GenerateFeedbackLink("bug: Panic in cerbosctl", util.Version, util.Commit, debug.Stack()) + "\n")
+			cmd.PrintErrln(string(debug.Stack()))
+
+			os.Exit(1)
+		}
+	}()
+
 	if err := cmd.Execute(); err != nil {
 		cmd.PrintErrf("ERROR: %v\n", err)
-		os.Exit(1)
+		os.Exit(1) //nolint:gocritic
 	}
 }
 
