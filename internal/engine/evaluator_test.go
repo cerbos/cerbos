3ba33df6bfc8da201f50045eeb2d3f212c449239
Dennis Buduev <dennis@cerbos.dev>
2021-11-19 18:44:41 +1300

    WIP

--- internal/engine/evaluator_test.go
+++ internal/engine/evaluator_test.go
@@ -1,35 +1,64 @@
 package engine
 
 import (
+	"fmt"
+	enginev1 "github.com/cerbos/cerbos/api/genpb/cerbos/engine/v1"
 	requestv1 "github.com/cerbos/cerbos/api/genpb/cerbos/request/v1"
-	responsev1 "github.com/cerbos/cerbos/api/genpb/cerbos/response/v1"
 	runtimev1 "github.com/cerbos/cerbos/api/genpb/cerbos/runtime/v1"
-	"reflect"
+	"github.com/cerbos/cerbos/internal/conditions"
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/parser"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/structpb"
 	"testing"
 )
 
 func Test_evaluateCondition(t *testing.T) {
 	type args struct {
-		condition *runtimev1.Condition
+		expr      string
 		input     *requestv1.ListResourcesRequest
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    *responsev1.ListResourcesResponse_Node
-		wantErr bool
+		args           args
+		wantExpression string
 	}{
-		// TODO: Add test cases.
+		{
+			args: args{
+				expr: "false",
+				input: &requestv1.ListResourcesRequest{},
+			},
+			wantExpression: "false",
+		},
+		{
+			args: args{
+				expr: "P.attr.authenticated",
+				input: &requestv1.ListResourcesRequest{
+					Principal:    &enginev1.Principal{
+						Attr:          map[string]*structpb.Value{"authenticated": {Kind: &structpb.Value_BoolValue{BoolValue: true}}},
+					},
+				},
+			},
+			wantExpression: "true",
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := evaluateCondition(tt.args.condition, tt.args.input)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("evaluateCondition() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("evaluateCondition() got = %v, want %v", got, tt.want)
+		t.Run(fmt.Sprintf("Expr:%q", tt.args.expr), func(t *testing.T) {
+			is := require.New(t)
+			ast, iss := conditions.StdEnv.Compile(tt.args.expr)
+			is.Nil(iss, "Error is %s", iss.Err())
+			checkedExpr, err := cel.AstToCheckedExpr(ast)
+			c := &runtimev1.Condition{Op: &runtimev1.Condition_Expr{Expr: &runtimev1.Expr{
+				Original: tt.args.expr,
+				Checked: checkedExpr,
+			}}}
+			got, err := evaluateCondition(c, tt.args.input)
+			is.NoError(err)
+			if tt.wantExpression != "" {
+				expression := got.GetExpression()
+				is.NotNil(expression)
+				source, err := parser.Unparse(expression.Expr, expression.SourceInfo)
+				is.NoError(err)
+				is.Equal(tt.wantExpression, source)
 			}
 		})
 	}
