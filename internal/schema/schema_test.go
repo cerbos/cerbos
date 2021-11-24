652a456d0d07b26fbf3e9a95d2004bd9670c686b
Oğuzhan D <oguzhandurgun95@gmail.com>
2021-11-24 21:59:51 +0300

    tests
    Signed-off-by: Oğuzhan D <oguzhandurgun95@gmail.com>

--- internal/schema/schema_test.go
+++ internal/schema/schema_test.go
@@ -69,6 +69,19 @@ func TestValidate(t *testing.T) {
 				},
 				wantErr: false,
 			},
+			{
+				name: "Test 2 - Nil Check",
+				input: enginev1.CheckInput{
+					Principal: &enginev1.Principal{
+						Attr: nil,
+					},
+					Resource: &enginev1.Resource{
+						Attr: nil,
+					},
+					Actions: []string{"view"},
+				},
+				wantErr: false,
+			},
 		}
 
 		var mgr = &Manager{
@@ -146,6 +159,12 @@ func TestMethods(t *testing.T) {
 				},
 				wantErr: false,
 			},
+			{
+				name:    "Test 2 - Nil Check",
+				input:   nil,
+				want:    nil,
+				wantErr: false,
+			},
 		}
 
 		for _, tc := range testCases {
@@ -167,19 +186,23 @@ func TestMethods(t *testing.T) {
 		mgr := &Manager{}
 
 		var testCases = []struct {
-			name        string
-			inputSchema string
-			want        map[string]string
-			wantErr     bool
+			name          string
+			inputSchema   string
+			wantPrincipal map[string]string
+			wantResource  map[string]string
+			wantErr       bool
 		}{
 			{
 				name:        "Test 1",
 				inputSchema: "LS0tCmFwaVZlcnNpb246ICJhcGkuY2VyYm9zLmRldi92MSIKZGVzY3JpcHRpb246IHwtCiBTY2hlbWEgZGVmaW5pdGlvbiBmaWxlCgpwcmluY2lwYWxTY2hlbWE6ICMgZGVmaW5lIHNjaGVtYSBmb3IgcmVxdWVzdC5wcmluY2lwYWwuYXR0cgogIHR5cGU6IG9iamVjdAogIHByb3BlcnRpZXM6CiAgICBwZXJtaXNzaW9uczoKICAgICAgdHlwZTogYXJyYXkKICAgICAgZGVzY3JpcHRpb246ICJwZXJtaXNzaW9ucyBkZWZpbmVkIGZvciB0aGlzIHVzZXIiCiAgICBlZGl0b3JfZm9yX2NhdGVnb3JpZXM6CiAgICAgIHR5cGU6IGFycmF5CiAgICBwZXJzb25hbF9kZXRhaWxzOgogICAgICB0eXBlOiBvYmplY3QKICAgICAgcHJvcGVydGllczoKICAgICAgICBuYW1lOgogICAgICAgICAgdHlwZTogc3RyaW5nCiAgICAgICAgbWFpbDoKICAgICAgICAgIHR5cGU6IHN0cmluZwoKcmVzb3VyY2VTY2hlbWFzOiAjIGRlZmluZSBzY2hlbWEgZm9yIHJlcXVlc3QucmVzb3VyY2UuYXR0cgogIHJlc291cmNlX2Jsb2c6CiAgICB0eXBlOiBvYmplY3QKICAgIHByb3BlcnRpZXM6CiAgICAgIGNhdGVnb3JpZXM6CiAgICAgICAgdHlwZTogc3RyaW5nCg==",
-				want: map[string]string{
+				wantPrincipal: map[string]string{
 					"/permissions":           "/permissions",
 					"/personal_details/mail": "/personal_details/mail",
 					"/personal_details/name": "/personal_details/name",
 					"/editor_for_categories": "/editor_for_categories",
+				},
+				wantResource: map[string]string{
+					"/categories": "/categories",
 				},
 				wantErr: false,
 			},
@@ -192,16 +215,29 @@ func TestMethods(t *testing.T) {
 			sch, err := ReadSchema(bytes.NewReader(schemaString))
 			require.NoError(t, err)
 
-			var have = make(map[string]string)
-			mgr.walkSchemaProperties("", sch.PrincipalSchema.Properties, have)
+			var havePrincipal = make(map[string]string)
+			mgr.walkSchemaProperties("", sch.PrincipalSchema.Properties, havePrincipal)
 
-			eq := mapsEqual(tc.want, have)
+			eq := mapsEqual(tc.wantPrincipal, havePrincipal)
 			if !eq && !tc.wantErr {
 				t.Log("wanted map is not equal to resulting map")
 				t.Fail()
 			} else if eq && tc.wantErr {
 				t.Log("wanted map is equal to resulting map, but error expected")
 				t.Fail()
+			}
+
+			var haveResource = make(map[string]string)
+			for _, resourceSchema := range sch.ResourceSchemas {
+				mgr.walkSchemaProperties("", resourceSchema.Properties, haveResource)
+				eq = mapsEqual(tc.wantResource, haveResource)
+				if !eq && !tc.wantErr {
+					t.Log("wanted map is not equal to resulting map")
+					t.Fail()
+				} else if eq && tc.wantErr {
+					t.Log("wanted map is equal to resulting map, but error expected")
+					t.Fail()
+				}
 			}
 		}
 	})
@@ -253,6 +289,39 @@ func TestMethods(t *testing.T) {
 					"/somekey3":         "/somekey3",
 				},
 				wantErr: false,
+			},
+			{
+				name: "Test 2 - Extra fields provided in input",
+				inputProperties: map[string]*structpb.Value{
+					"somekey": {
+						Kind: &structpb.Value_StructValue{
+							StructValue: &structpb.Struct{
+								Fields: map[string]*structpb.Value{
+									"somekey1": {
+										Kind: &structpb.Value_StringValue{
+											StringValue: "somevalue1",
+										},
+									},
+									"somekey2": {
+										Kind: &structpb.Value_NumberValue{
+											NumberValue: 2,
+										},
+									},
+								},
+							},
+						},
+					},
+					"somekey3": {
+						Kind: &structpb.Value_BoolValue{
+							BoolValue: true,
+						},
+					},
+				},
+				schemaProperties: map[string]string{
+					"/somekey/somekey1": "/somekey/somekey1",
+					"/somekey/somekey2": "/somekey/somekey2",
+				},
+				wantErr: true,
 			},
 		}
 
