deb41872cb4d35d4993c54fc642bbb3dc62d2633
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2021-12-23 16:01:42 +0300

    Fix extra spaces in defaultValues
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- internal/storage/db/mysql/conf.go
+++ internal/storage/db/mysql/conf.go
@@ -14,9 +14,9 @@ const confKey = storage.ConfKey + ".mysql"
 type Conf struct {
 	// Data source name
 	DSN          string                 `yaml:"dsn" conf:"required,defaultValue=\"user:password@tcp(localhost:3306)/db?interpolateParams=true\""`
-	ConnPool     *internal.ConnPoolConf `yaml:"connPool" conf:",defaultValue=\n      maxLifeTime: 60m\n      maxIdleTime: 45s\n      maxOpen: 4\n      maxIdle: 1"`
-	TLS          map[string]TLSConf     `yaml:"tls" conf:",defaultValue=\n        mytls:\n          cert: /path/to/certificate\n          key: /path/to/private_key\n          caCert: /path/to/CA_certificate"`
-	ServerPubKey map[string]string      `yaml:"serverPubKey" conf:",defaultValue=\n        mykey: testdata/server_public_key.pem"`
+	ConnPool     *internal.ConnPoolConf `yaml:"connPool" conf:",defaultValue=\n  maxLifeTime: 60m\n  maxIdleTime: 45s\n  maxOpen: 4\n  maxIdle: 1"`
+	TLS          map[string]TLSConf     `yaml:"tls" conf:",defaultValue=\n  mytls:\n    cert: /path/to/certificate\n    key: /path/to/private_key\n    caCert: /path/to/CA_certificate"`
+	ServerPubKey map[string]string      `yaml:"serverPubKey" conf:",defaultValue=\n  mykey: testdata/server_public_key.pem"`
 }
 
 type TLSConf struct {
