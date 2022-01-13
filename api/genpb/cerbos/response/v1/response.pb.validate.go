5c7495f4e692f53a4d3db2dfddf40c5da07b004f
Dennis Buduev <dennis@cerbos.dev>
2022-01-13 13:01:24 +1300

    feat!: Change ReseourcesQueryPlanResponse.Filter
    Signed-off-by: Dennis Buduev <dennis@cerbos.dev>
    
    Co-authored-by: Charith Ellawala <charithe@users.noreply.github.com>
    Signed-off-by: Dennis Buduev <dennis@cerbos.dev>

--- api/genpb/cerbos/response/v1/response.pb.validate.go
+++ api/genpb/cerbos/response/v1/response.pb.validate.go
@@ -2413,6 +2413,141 @@ var _ interface {
 	ErrorName() string
 } = ResourcesQueryPlanResponse_ExpressionValidationError{}
 
+// Validate checks the field values on ResourcesQueryPlanResponse_Filter with
+// the rules defined in the proto definition for this message. If any rules
+// are violated, the first error encountered is returned, or nil if there are
+// no violations.
+func (m *ResourcesQueryPlanResponse_Filter) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ResourcesQueryPlanResponse_Filter
+// with the rules defined in the proto definition for this message. If any
+// rules are violated, the result is a list of violation errors wrapped in
+// ResourcesQueryPlanResponse_FilterMultiError, or nil if none found.
+func (m *ResourcesQueryPlanResponse_Filter) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ResourcesQueryPlanResponse_Filter) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Kind
+
+	if all {
+		switch v := interface{}(m.GetCondition()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ResourcesQueryPlanResponse_FilterValidationError{
+					field:  "Condition",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ResourcesQueryPlanResponse_FilterValidationError{
+					field:  "Condition",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetCondition()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ResourcesQueryPlanResponse_FilterValidationError{
+				field:  "Condition",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		return ResourcesQueryPlanResponse_FilterMultiError(errors)
+	}
+	return nil
+}
+
+// ResourcesQueryPlanResponse_FilterMultiError is an error wrapping multiple
+// validation errors returned by
+// ResourcesQueryPlanResponse_Filter.ValidateAll() if the designated
+// constraints aren't met.
+type ResourcesQueryPlanResponse_FilterMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ResourcesQueryPlanResponse_FilterMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ResourcesQueryPlanResponse_FilterMultiError) AllErrors() []error { return m }
+
+// ResourcesQueryPlanResponse_FilterValidationError is the validation error
+// returned by ResourcesQueryPlanResponse_Filter.Validate if the designated
+// constraints aren't met.
+type ResourcesQueryPlanResponse_FilterValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ResourcesQueryPlanResponse_FilterValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ResourcesQueryPlanResponse_FilterValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ResourcesQueryPlanResponse_FilterValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ResourcesQueryPlanResponse_FilterValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ResourcesQueryPlanResponse_FilterValidationError) ErrorName() string {
+	return "ResourcesQueryPlanResponse_FilterValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ResourcesQueryPlanResponse_FilterValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sResourcesQueryPlanResponse_Filter.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ResourcesQueryPlanResponse_FilterValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ResourcesQueryPlanResponse_FilterValidationError{}
+
 // Validate checks the field values on ResourcesQueryPlanResponse_Meta with the
 // rules defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
