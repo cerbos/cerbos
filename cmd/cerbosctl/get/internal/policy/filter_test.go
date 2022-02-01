1288d60df3fb43fac91692f468b58356e5f39be0
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2022-02-01 19:01:24 +0300

    Add filter_test.go
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- cmd/cerbosctl/get/internal/policy/filter_test.go
+++ cmd/cerbosctl/get/internal/policy/filter_test.go
@@ -8,39 +8,123 @@ package policy
 
 import (
 	"fmt"
-	"github.com/cerbos/cerbos/cmd/cerbosctl/get/internal/flagset"
-	"github.com/cerbos/cerbos/internal/policy"
-	"github.com/cerbos/cerbos/internal/test"
-	"github.com/stretchr/testify/require"
 	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cerbos/cerbos/internal/policy"
+	"github.com/cerbos/cerbos/internal/test"
 )
 
-const derivedRolesFmt = "derived_roles.my_derived_roles_%d"
+const (
+	derivedRolesNameFmt      = "my_derived_roles_%d"
+	principalPoliciesNameFmt = "donald_duck_%d"
+	resourcePoliciesNameFmt  = "leave_request_%d"
+	derivedRolesFmt          = "derived_roles.my_derived_roles_%d"
+	principalPoliciesFmt     = "principal.donald_duck_%d.version"
+	resourcePoliciesFmt      = "resource.leave_request_%d.default"
+	version                  = "default"
+)
 
-func TestSort(t *testing.T) {
-	t.Run("Sort by policyId", func(t *testing.T) {
-		noOfPolicies := 3
-		policies := mkPolicies(t, noOfPolicies)
-		expected := make([]string, noOfPolicies)
-		for i := 0; i < noOfPolicies; i++ {
-			expected[i] = fmt.Sprintf(derivedRolesFmt, i)
-		}
+func TestFilter(t *testing.T) {
+	noOfPolicies := 2
+	derivedRoles := mkDerivedRolesForFilter(t, noOfPolicies)
+	principalPolicies := mkPrincipalPoliciesForFilter(t, noOfPolicies)
+	resourcePolicies := mkResourcePoliciesForFilter(t, noOfPolicies)
 
-		sorted := sort(policies, flagset.SortByPolicyID)
-		require.NotEmpty(t, sorted)
-		for idx, p := range sorted {
-			require.Equal(t, expected[idx], p.Metadata.StoreIdentifer)
-		}
+	t.Run("should fail when filtering wrong kind of policies", func(t *testing.T) {
+		filtered := filter(derivedRoles, nil, nil, policy.PrincipalKind)
+		require.Empty(t, filtered)
+		filtered = filter(derivedRoles, nil, nil, policy.ResourceKind)
+		require.Empty(t, filtered)
+
+		filtered = filter(principalPolicies, nil, nil, policy.DerivedRolesKind)
+		require.Empty(t, filtered)
+		filtered = filter(principalPolicies, nil, nil, policy.ResourceKind)
+		require.Empty(t, filtered)
+
+		filtered = filter(resourcePolicies, nil, nil, policy.DerivedRolesKind)
+		require.Empty(t, filtered)
+		filtered = filter(resourcePolicies, nil, nil, policy.PrincipalKind)
+		require.Empty(t, filtered)
+	})
+
+	// Derived Roles
+	t.Run("should filter derived_roles by kind", func(t *testing.T) {
+		filtered := filter(derivedRoles, nil, nil, policy.DerivedRolesKind)
+		require.Len(t, filtered, noOfPolicies)
+	})
+
+	t.Run("should filter derived_roles by name", func(t *testing.T) {
+		filtered := filter(derivedRoles, []string{fmt.Sprintf(derivedRolesNameFmt, 1)}, nil, policy.DerivedRolesKind)
+		require.Len(t, filtered, 1)
+		require.Equal(t, fmt.Sprintf(derivedRolesNameFmt, 1), filtered[0].Name)
+	})
+
+	// Principal Policies
+	t.Run("should filter principal_policies by kind", func(t *testing.T) {
+		filtered := filter(principalPolicies, nil, nil, policy.PrincipalKind)
+		require.Len(t, filtered, noOfPolicies)
+	})
+
+	t.Run("should filter principal_policies by name", func(t *testing.T) {
+		filtered := filter(principalPolicies, []string{fmt.Sprintf(principalPoliciesNameFmt, 1)}, nil, policy.PrincipalKind)
+		require.Len(t, filtered, 1)
+		require.Equal(t, fmt.Sprintf(principalPoliciesNameFmt, 1), filtered[0].Name)
+	})
+
+	t.Run("should filter principal_policies by version", func(t *testing.T) {
+		filtered := filter(principalPolicies, nil, []string{version}, policy.PrincipalKind)
+		require.Len(t, filtered, noOfPolicies)
+		require.Equal(t, version, filtered[0].Version)
+	})
+
+	// Resource Policies
+	t.Run("should filter resource_policies by kind", func(t *testing.T) {
+		filtered := filter(resourcePolicies, nil, nil, policy.ResourceKind)
+		require.Len(t, filtered, noOfPolicies)
+	})
+
+	t.Run("should filter resource_policies by name", func(t *testing.T) {
+		filtered := filter(resourcePolicies, []string{fmt.Sprintf(resourcePoliciesNameFmt, 1)}, nil, policy.ResourceKind)
+		require.Len(t, filtered, 1)
+		require.Equal(t, fmt.Sprintf(resourcePoliciesNameFmt, 1), filtered[0].Name)
+	})
+
+	t.Run("should filter resource_policies by version", func(t *testing.T) {
+		filtered := filter(resourcePolicies, nil, []string{version}, policy.ResourceKind)
+		require.Len(t, filtered, noOfPolicies)
+		require.Equal(t, version, filtered[0].Version)
 	})
 }
 
-func mkPolicies(t *testing.T, noOfPolicies int) []policy.Wrapper {
+func mkDerivedRolesForFilter(t *testing.T, noOfPolicies int) []policy.Wrapper {
 	t.Helper()
 
 	policies := make([]policy.Wrapper, noOfPolicies)
-	for i := noOfPolicies-1; i >= 0; i-- {
+	for i := noOfPolicies - 1; i >= 0; i-- {
 		policies[i] = policy.Wrap(policy.WithStoreIdentifier(test.GenDerivedRoles(test.Suffix(strconv.Itoa(i))), fmt.Sprintf(derivedRolesFmt, i)))
+	}
+	return policies
+}
+
+func mkPrincipalPoliciesForFilter(t *testing.T, noOfPolicies int) []policy.Wrapper {
+	t.Helper()
+
+	policies := make([]policy.Wrapper, noOfPolicies)
+	for i := noOfPolicies - 1; i >= 0; i-- {
+		policies[i] = policy.Wrap(policy.WithStoreIdentifier(test.GenPrincipalPolicy(test.Suffix(strconv.Itoa(i))), fmt.Sprintf(principalPoliciesFmt, i)))
+	}
+	return policies
+}
+
+func mkResourcePoliciesForFilter(t *testing.T, noOfPolicies int) []policy.Wrapper {
+	t.Helper()
+
+	policies := make([]policy.Wrapper, noOfPolicies)
+	for i := noOfPolicies - 1; i >= 0; i-- {
+		policies[i] = policy.Wrap(policy.WithStoreIdentifier(test.GenResourcePolicy(test.Suffix(strconv.Itoa(i))), fmt.Sprintf(resourcePoliciesFmt, i)))
 	}
 	return policies
 }
