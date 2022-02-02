b725a10f51ef3ba7a1889d39758684e9afa85a57
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2022-02-02 14:06:41 +0300

    Add index_test.go
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

---
+++ internal/storage/index/index_test.go
@@ -0,0 +1,97 @@
+// Copyright 2021-2022 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build tests
+// +build tests
+
+package index_test
+
+import (
+	"context"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/cerbos/cerbos/internal/policy"
+	"github.com/cerbos/cerbos/internal/schema"
+	"github.com/cerbos/cerbos/internal/storage/index"
+	"github.com/cerbos/cerbos/internal/test"
+	"github.com/cerbos/cerbos/internal/util"
+)
+
+func TestIndex(t *testing.T) {
+	fsDir := test.PathToDir(t, "store")
+	fsys := os.DirFS(fsDir)
+	idx, err := index.Build(context.Background(), fsys)
+	require.NoError(t, err)
+
+	policyFiles := mkListOfFiles(t, fsys)
+
+	t.Run("load policy", func(t *testing.T) {
+		t.Run("should load the policies", func(t *testing.T) {
+			policies, err := idx.LoadPolicy(context.Background(), policyFiles...)
+			require.NoError(t, err)
+			require.Len(t, policies, len(policyFiles))
+		})
+
+		t.Run("should have not empty metadata in the policies", func(t *testing.T) {
+			policies, err := idx.LoadPolicy(context.Background(), policyFiles...)
+			require.NoError(t, err)
+
+			for _, p := range policies {
+				require.NotEmpty(t, p.Metadata)
+			}
+		})
+
+		t.Run("should have the store identifier in the metadata of the policies", func(t *testing.T) {
+			policies, err := idx.LoadPolicy(context.Background(), policyFiles...)
+			require.NoError(t, err)
+
+			for idx, p := range policies {
+				require.Equal(t, policyFiles[idx], p.Metadata.StoreIdentifer)
+			}
+		})
+
+		t.Run("should have the hash in the metadata of the policies", func(t *testing.T) {
+			policies, err := idx.LoadPolicy(context.Background(), policyFiles...)
+			require.NoError(t, err)
+
+			for _, p := range policies {
+				require.Equal(t, wrapperspb.UInt64(util.HashPB(p, policy.IgnoreHashFields)), p.Metadata.Hash)
+			}
+		})
+	})
+}
+
+// mkListOfFiles generates a list of policy files under the given filesystem and returns the list.
+func mkListOfFiles(t *testing.T, fsys fs.FS) []string {
+	t.Helper()
+
+	var list []string
+	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			switch d.Name() {
+			case util.TestDataDirectory:
+				return fs.SkipDir
+			case schema.Directory:
+				return fs.SkipDir
+			default:
+				return nil
+			}
+		}
+
+		if !util.IsSupportedFileType(d.Name()) {
+			return nil
+		}
+
+		list = append(list, path)
+
+		return nil
+	})
+	require.NoError(t, err)
+
+	return list
+}
