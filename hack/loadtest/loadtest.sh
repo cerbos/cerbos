210e04bea429ed773d0da210cceac4c4d181a77f
Charith Ellawala <charithe@users.noreply.github.com>
2022-02-25 09:36:54 +0000

    chore(test): Tweak load test script (#675)
    * chore(test): Update load test script
    
    Modify to run a single case at a time.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Refactor script
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Fix policy path
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- hack/loadtest/loadtest.sh
+++ hack/loadtest/loadtest.sh
@@ -5,35 +5,26 @@
 
 set -euo pipefail
 
-trap onExit EXIT
+WORK_DIR="work"
+
+# Test parameters
+DURATION=${DURATION:-"120s"}
+MAX_VUS=${MAX_VUS:-"100"}
+MIN_VUS=${MIN_VUS:-"25"}
+NUM_POLICIES=${NUM_POLICIES:-"1000"}
+RPS=${RPS:-"200"}
+STORE=${STORE:-"disk"}
+
 clean() {
   printf "Cleaning up\n"
   rm -rf ./bin
-  rm -rf ./k6/gen
-  rm -rf ./conf/cerbos/config.yml
-  rm -rf ./data_postgres
-}
-
-onExit() {
-  down
-  clean
+  rm -rf work
 }
 
 generateResources() {
-  printf "Generating %s of policy sets\n" "${1}"
-  mkdir -p ./k6/gen/policies ./k6/gen/requests
-  go run ./genres.go --output-dir ./k6/gen --policy-set-count "${1}"
-}
-
-setupStore() {
-  printf "Setting configuration for store %s\n" "${1}"
-  cp ./conf/cerbos/"${1}".yml ./conf/cerbos/config.yml
-}
-
-setupPostgres() {
-  printf "Setting up the postgres schema\n"
-  docker cp ../../internal/storage/db/postgres/schema.sql postgres:/docker-entrypoint-initdb.d/schema.sql
-  docker exec -u postgres postgres psql cerbos cerbos -f docker-entrypoint-initdb.d/schema.sql
+  printf "Generating %s policy sets\n" "$NUM_POLICIES"
+  mkdir -p "${WORK_DIR}"/k6/{policies,requests}
+  go run ./genres.go --output-dir "${WORK_DIR}/k6" --policy-set-count "$NUM_POLICIES"
 }
 
 down() {
@@ -42,50 +33,96 @@ down() {
 }
 
 up() {
+  printf "Preparing config\n"
+  mkdir -p "${WORK_DIR}/cerbos"
+  mkdir -p "${WORK_DIR}"/postgres/{init,data}
+  cp ../../internal/storage/db/postgres/schema.sql "${WORK_DIR}/postgres/init/schema.sql"
+
+  case "$STORE" in 
+    disk)
+      cp conf/cerbos/disk.yml "${WORK_DIR}/cerbos/config.yml"
+      ;;
+
+    postgres)
+      cp conf/cerbos/postgres.yml "${WORK_DIR}/cerbos/config.yml"
+      ;;
+
+    *)
+      echo "Unknown store '$STORE'. Valid values are: 'disk', 'postgres'"
+      usage
+      ;;
+  esac
+
   printf "Starting all services\n"
   docker-compose up -d
-  sleep "${1}"
+
+  while [[ "$(curl -s -o /dev/null -w '%{http_code}' 'http://localhost:3592/_cerbos/health')" != "200" ]]; do 
+      echo "Waiting for Cerbos..."
+      sleep 1 
+  done
+
+  docker-compose logs -f 
 }
 
 executeTest() {
-  generateResources "${1}"
-  k6 run ./k6/check.js>./results/"${2}"_"${1}".log 2>&1
+  printf "Store=%s NumPolicies=%s RPS=%s DURATION=%s MIN_VUS=%s MAX_VUS=%s\n", $STORE, $NUM_POLICIES, $RPS, $DURATION, $MIN_VUS, $MAX_VUS
+  mkdir -p results
+  k6 run \
+    --out json="results/${STORE}_${NUM_POLICIES}.json" \
+    -e DURATION="$DURATION" \
+    -e MAX_VUS="$MAX_VUS" \
+    -e MIN_VUS="$MIN_VUS" \
+    -e RPS="$RPS" \
+    ./k6/check.js
 }
 
-down
-clean
-rm -rf ./results
+usage() {
+  printf "Usage:\n%s [-c | -d | -e | -h | -u ]\n", "$0"
+  printf "Flags:\n"
+  printf "\t-c Cleanup\n"
+  printf "\t-d Down (stop services)\n"
+  printf "\t-e Execute test\n"
+  printf "\t-h Help\n"
+  printf "\t-u Up (start services)\n"
+}
 
-printf "Creating results folder\n"
-mkdir ./results
 
-setupStore "disk"
-generateResources 1
-up 20
-setupPostgres
-down
-
-stores=("disk" "postgres")
-scenarios=( 5 50 500 5000 )
-
-printf "Starting the load tests\n\n"
-
-for store in "${stores[@]}"
-do
-  down
-
-  printf "Preparing for the load test with %s store\n" "${store}"
-  setupStore "${store}"
-
-  printf "Starting all services\n"
-  up 5
-
-  for i in "${scenarios[@]}"
-  do
-    :
-    printf "Executing the load test with %s store and %s number of policy sets\n" "${store}" "${i}"
-    executeTest "${i}" "${store}"
-  done
-
-  printf "Load test with %s store finalized\n\n\n" "${store}"
+while getopts ":cdehu" opt; do 
+  case "$opt" in
+    c) 
+      down
+      clean
+      exit 0
+      ;;
+    d)
+      down
+      exit 0
+      ;;
+    e)
+      executeTest
+      exit 0
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    u)
+      generateResources
+      up
+      exit 0
+      ;;
+    \?)
+      echo "Unknown option $OPTARG"
+      usage
+      exit 2
+      ;;
+    :)  
+      echo "Flag $OPTARG requires an argument"
+      usage
+      exit 2
+      ;;
+  esac
 done
+
+usage
+exit 2
