41d95264f366f056416bdb6389edc94c507d4e7e
Charith Ellawala <charith@cerbos.dev>
2022-03-10 15:14:52 +0000

    Parameterise ghz script
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- hack/loadtest/ghz/crb_req01.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ghz --skipTLS --insecure --duration=30s --concurrency=100 --connections=100 --load-schedule=const --rps=500 --call cerbos.svc.v1.CerbosService/CheckResourceBatch -D crb_req01.json localhost:3593
-ghz --skipTLS --insecure --total=1000000 --concurrency=100 --connections=100 --call cerbos.svc.v1.CerbosService/CheckResourceBatch -D crb_req01.json localhost:3593
-
--- hack/loadtest/ghz/crs_req01.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ghz --skipTLS --insecure --duration=30s --concurrency=100 --connections=100 --load-schedule=const --rps=500 --call cerbos.svc.v1.CerbosService/CheckResourceSet -D crs_req01.json localhost:3593
-ghz --skipTLS --insecure --total=1000000 --concurrency=100 --connections=100 --call cerbos.svc.v1.CerbosService/CheckResourceSet -D crs_req01.json localhost:3593
-
