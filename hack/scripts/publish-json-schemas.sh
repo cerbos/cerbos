6e3f7a2558ae7d043c6f71f79e05ff9e581de2de
Andrew Haines <haines@cerbos.dev>
2022-03-16 16:17:38 +0000

    feat: Publish JSON schemas
    Schemas will be available for each release at https://api.cerbos.dev/vX.Y.Z,
    and for the latest release at https://api.cerbos.dev/latest.
    
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

---
+++ hack/scripts/publish-json-schemas.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Copyright 2021-2022 Zenauth Ltd.
+
+set -euo pipefail
+
+if [[ $# -ne 2 || -z "$1" || -z "$2" ]]; then
+    echo "Usage: $0 <version> <gcs-bucket>"
+    exit 2
+fi
+
+VERSION="$1"
+GCS_BUCKET="$2"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SOURCE_JSON_SCHEMAS_DIR="${SCRIPT_DIR}/../../schema/jsonschema"
+TARGET_JSON_SCHEMAS_DIR="$(mktemp -d -t cerbos-XXXXX)"
+trap 'rm -rf "$TARGET_JSON_SCHEMAS_DIR"' EXIT
+
+(
+    cd "$SOURCE_JSON_SCHEMAS_DIR"
+    find . -type f -name "*.schema.json" -exec "${SCRIPT_DIR}/set-json-schema-version.sh" "$VERSION" "{}" "${TARGET_JSON_SCHEMAS_DIR}/{}" \;
+)
+
+gsutil -m -h "Content-Type: application/schema+json" rsync -r "$TARGET_JSON_SCHEMAS_DIR" "gs://${GCS_BUCKET}/${VERSION}"
---
+++ hack/scripts/set-json-schema-version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Copyright 2021-2022 Zenauth Ltd.
+
+set -euo pipefail
+
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 <version> <input-file> <output-file>"
+    exit 2
+fi
+
+VERSION="$1"
+INPUT_FILE="$2"
+OUTPUT_FILE="$3"
+OUTPUT_DIR="$(dirname "$OUTPUT_FILE")"
+
+mkdir -p "$OUTPUT_DIR"
+jq --arg version "$VERSION" '.["$id"] |= sub("(?<=https://api.cerbos.dev/)"; $version + "/")' < "$INPUT_FILE" > "$OUTPUT_FILE"
