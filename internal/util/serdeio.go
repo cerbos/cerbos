976e8199fbc76c359634c4b856245b9390b5e85d
Charith Ellawala <charithe@users.noreply.github.com>
2022-03-31 09:37:59 +0100

    fix: Handle comments at the beginning of YAML files (#803)
    * fix: Handle comments at the beginning of YAML files
    
    If the first line of the YAML file contains a comment, the
    multiple-document detector incorrectly classifies the file as containing
    more than one YAML document and fails. This patch fixes that issue.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    * Handle spaces at the beginning
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- internal/util/serdeio.go
+++ internal/util/serdeio.go
@@ -25,6 +25,7 @@ const (
 var (
 	jsonStart           = []byte("{")
 	yamlSep             = []byte("---")
+	yamlComment         = []byte("#")
 	ErrMultipleYAMLDocs = errors.New("more than one YAML document detected")
 )
 
@@ -78,9 +79,15 @@ func newYAMLDecoder(src *bufio.Reader) decoderFunc {
 		seenContent := false
 		for s.Scan() {
 			line := s.Bytes()
+			trimmedLine := bytes.TrimSpace(line)
+
+			// ignore comments
+			if bytes.HasPrefix(trimmedLine, yamlComment) {
+				continue
+			}
 
 			// ignore empty lines at the beginning of the file
-			if !seenContent && len(bytes.TrimSpace(line)) == 0 {
+			if !seenContent && len(trimmedLine) == 0 {
 				continue
 			}
 			seenContent = true
