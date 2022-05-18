58418225500e96657298095444e38b8cc8360e9b
Dennis Buduev <dennis@cerbos.dev>
2022-05-18 22:19:42 +1200

    WIP

--- internal/wasm/gen_test.go
+++ internal/wasm/gen_test.go
@@ -18,6 +18,7 @@ type Property struct {
 	Type     string
 	Name     string
 	Required bool
+	ItemType string
 }
 
 func TestClasses(t *testing.T) {
@@ -27,9 +28,9 @@ func TestClasses(t *testing.T) {
 	is.NoError(err)
 	is.NotNil(tmpl)
 	props := []Property{
-		{"vec", "amount", true},
-		{"string", "department", true},
-		{"string", "team", false},
+		{"Vec", "amount", true, "String"},
+		{"String", "department", true, ""},
+		{"String", "team", false, ""},
 	}
 	tmpl.ExecuteTemplate(os.Stdout, "request", props)
 }
