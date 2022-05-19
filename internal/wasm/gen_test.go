176e12b29ba6d3ca8cd6b0feeea54ea0743bdf78
Dennis Buduev <dennis@cerbos.dev>
2022-05-19 15:30:46 +1200

    WIP: check

--- internal/wasm/gen_test.go
+++ internal/wasm/gen_test.go
@@ -10,15 +10,12 @@ import (
 	"path"
 )
 
-//go:embed templates/*.rs.tmpl
+//go:embed templates/*.tmpl
 var templatesFS embed.FS
-
-/*
- */
 
 func TestClasses(t *testing.T) {
 	is := require.New(t)
-	tmpl, err := template.ParseFS(templatesFS, "templates/*.rs.tmpl")
+	tmpl, err := template.ParseFS(templatesFS, "templates/*.tmpl")
 
 	is.NoError(err)
 	is.NotNil(tmpl)
@@ -27,4 +24,29 @@ func TestClasses(t *testing.T) {
 	props, err := convert(s)
 	is.NoError(err)
 	tmpl.ExecuteTemplate(os.Stdout, "lib", props)
+}
+
+type Policy struct {
+	Rules []Rule
+}
+
+type Rule struct {
+	Roles   []string
+	Actions []string
+}
+
+func TestCheck(t *testing.T) {
+	is := require.New(t)
+	tmpl, err := template.ParseFS(templatesFS, "templates/*.tmpl")
+
+	is.NoError(err)
+	is.NotNil(tmpl)
+
+	policy := Policy{
+		Rules: []Rule{{
+			Roles:   []string{"admin", "manager"},
+			Actions: []string{"read", "update"},
+		}},
+	}
+	tmpl.ExecuteTemplate(os.Stdout, "check", policy)
 }
