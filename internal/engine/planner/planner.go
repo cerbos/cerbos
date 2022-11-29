b8f94e2db98d1a3033630f595c1e06c68fbcdff2
Dennis Buduev <dbuduev@users.noreply.github.com>
2022-11-29 13:49:26 +1300

    fix: R.id is unknown
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- internal/engine/planner/planner.go
+++ internal/engine/planner/planner.go
@@ -566,8 +566,8 @@ func newEvaluator(input *enginev1.PlanResourcesInput) (p *partialEvaluator, err 
 		}
 	}
 	p.vars, err = cel.PartialVars(knownVars,
-		cel.AttributePattern(conditions.CELResourceAbbrev).QualString(conditions.CELAttrField),
-		cel.AttributePattern(conditions.CELRequestIdent).QualString(conditions.CELResourceField).QualString(conditions.CELAttrField))
+		cel.AttributePattern(conditions.CELResourceAbbrev),
+		cel.AttributePattern(conditions.CELRequestIdent).QualString(conditions.CELResourceField))
 
 	if err != nil {
 		return nil, err
