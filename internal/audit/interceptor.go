41b4aaba3c809b10efe553464aac489a83ff6278
Charith Ellawala <charith@cerbos.dev>
2022-12-06 13:42:41 +0530

    Refactor metadata extraction
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- internal/audit/interceptor.go
+++ internal/audit/interceptor.go
@@ -5,13 +5,11 @@ package audit
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -25,15 +23,9 @@ type (
 )
 
 func NewUnaryInterceptor(log Log, exclude ExcludeMethod) (grpc.UnaryServerInterceptor, error) {
-	conf, err := GetConf()
+	mdExtractor, err := NewMetadataExtractor()
 	if err != nil {
-		return nil, fmt.Errorf("failed to read audit configuration: %w", err)
-	}
-
-	var keysPresent bool
-	var includeKeys IncludeKeysMethod
-	if keysPresent = !(len(conf.ExcludeMetadataKeys) == 0 && len(conf.IncludeMetadataKeys) == 0); keysPresent {
-		includeKeys = mkIncludeKeysMethod(conf.ExcludeMetadataKeys, conf.IncludeMetadataKeys)
+		return nil, err
 	}
 
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
@@ -54,55 +46,18 @@ func NewUnaryInterceptor(log Log, exclude ExcludeMethod) (grpc.UnaryServerInterc
 			ctx, span := tracing.StartSpan(ctx, "audit.WriteAccessLog")
 			defer span.End()
 
-			entry := &auditv1.AccessLogEntry{
+			return &auditv1.AccessLogEntry{
 				CallId:     string(callID),
 				Timestamp:  timestamppb.New(ts),
 				Peer:       PeerFromContext(ctx),
 				Method:     info.FullMethod,
 				StatusCode: uint32(status.Code(err)),
-			}
-
-			if keysPresent {
-				md, ok := metadata.FromIncomingContext(ctx)
-				if ok {
-					entry.Metadata = make(map[string]*auditv1.MetaValues, len(md))
-					for key, values := range md {
-						if includeKeys(key) {
-							entry.Metadata[key] = &auditv1.MetaValues{Values: values}
-						}
-					}
-				}
-			}
-
-			return entry, nil
+				Metadata:   mdExtractor(ctx),
+			}, nil
 		}); logErr != nil {
 			ctxzap.Extract(ctx).Warn("Failed to write access log entry", zap.Error(logErr))
 		}
 
 		return resp, err
 	}, nil
-}
-
-func mkIncludeKeysMethod(excludedMetadataKeys, includedMetadataKeys []string) IncludeKeysMethod {
-	exclude := sliceToLookupMap(excludedMetadataKeys)
-	include := sliceToLookupMap(includedMetadataKeys)
-	return func(key string) bool {
-		_, existsInExcludedKeys := exclude[key]
-		_, existsInIncludedKeys := include[key]
-
-		if !existsInExcludedKeys && existsInIncludedKeys {
-			return true
-		}
-
-		return false
-	}
-}
-
-func sliceToLookupMap(slice []string) map[string]struct{} {
-	m := make(map[string]struct{})
-	for _, k := range slice {
-		m[k] = struct{}{}
-	}
-
-	return m
 }
---
+++ internal/audit/metadata_test.go
@@ -0,0 +1,74 @@
+// Copyright 2021-2022 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package audit
+
+import (
+	"context"
+	"testing"
+
+	auditv1 "github.com/cerbos/cerbos/api/genpb/cerbos/audit/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestMetadataExtractor(t *testing.T) {
+	testCases := []struct {
+		name        string
+		includeKeys []string
+		excludeKeys []string
+		input       map[string]string
+		want        map[string]*auditv1.MetaValues
+	}{
+		{
+			name:  "NoIncludeOrExclude",
+			input: map[string]string{"foo": "a", "bar": "b"},
+		},
+		{
+			name:        "OnlyInclude",
+			includeKeys: []string{"foo", "bar"},
+			input:       map[string]string{"foo": "a", "bar": "b", "baz": "c"},
+			want: map[string]*auditv1.MetaValues{
+				"foo": {Values: []string{"a"}},
+				"bar": {Values: []string{"b"}},
+			},
+		},
+		{
+			name:        "OnlyExclude",
+			excludeKeys: []string{"foo", "bar"},
+			input:       map[string]string{"foo": "a", "bar": "b", "baz": "c"},
+			want: map[string]*auditv1.MetaValues{
+				"baz": {Values: []string{"c"}},
+			},
+		},
+		{
+			name:        "BothIncludeAndExclude",
+			includeKeys: []string{"foo"},
+			excludeKeys: []string{"bar"},
+			input:       map[string]string{"foo": "a", "bar": "b", "baz": "c"},
+			want: map[string]*auditv1.MetaValues{
+				"foo": {Values: []string{"a"}},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			conf := &Conf{
+				confHolder: confHolder{
+					IncludeMetadataKeys: tc.includeKeys,
+					ExcludeMetadataKeys: tc.excludeKeys,
+				},
+			}
+			me := NewMetadataExtractorFromConf(conf)
+			ctx := metadata.NewIncomingContext(context.Background(), metadata.New(tc.input))
+
+			have := me(ctx)
+			require.Len(t, have, len(tc.want))
+			require.True(t, cmp.Equal(tc.want, have, protocmp.Transform()))
+		})
+	}
+}
