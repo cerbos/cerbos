a15244eb80f9ad719e7e69dcb374d6e89cc9d2cb
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2023-02-28 17:01:16 +0300

    chore: Deprecate old policy disable endpoint
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- api/genpb/cerbos/svc/v1/svc.pb.gw.go
+++ api/genpb/cerbos/svc/v1/svc.pb.gw.go
@@ -397,6 +397,42 @@ func local_request_CerbosAdminService_DisablePolicy_1(ctx context.Context, marsh
 }
 
 var (
+	filter_CerbosAdminService_DisablePolicy_2 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_CerbosAdminService_DisablePolicy_2(ctx context.Context, marshaler runtime.Marshaler, client CerbosAdminServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq requestv1.DisablePolicyRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CerbosAdminService_DisablePolicy_2); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DisablePolicy(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_CerbosAdminService_DisablePolicy_2(ctx context.Context, marshaler runtime.Marshaler, server CerbosAdminServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq requestv1.DisablePolicyRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CerbosAdminService_DisablePolicy_2); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DisablePolicy(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+var (
 	filter_CerbosAdminService_EnablePolicy_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
@@ -1134,6 +1170,31 @@ func RegisterCerbosAdminServiceHandlerServer(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("DELETE", pattern_CerbosAdminService_DisablePolicy_2, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/cerbos.svc.v1.CerbosAdminService/DisablePolicy", runtime.WithHTTPPathPattern("/admin/policy"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_CerbosAdminService_DisablePolicy_2(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CerbosAdminService_DisablePolicy_2(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_CerbosAdminService_EnablePolicy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1798,6 +1859,28 @@ func RegisterCerbosAdminServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("DELETE", pattern_CerbosAdminService_DisablePolicy_2, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/cerbos.svc.v1.CerbosAdminService/DisablePolicy", runtime.WithHTTPPathPattern("/admin/policy"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_CerbosAdminService_DisablePolicy_2(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_CerbosAdminService_DisablePolicy_2(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_CerbosAdminService_EnablePolicy_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -2012,6 +2095,8 @@ var (
 
 	pattern_CerbosAdminService_DisablePolicy_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"admin", "policy", "disable"}, ""))
 
+	pattern_CerbosAdminService_DisablePolicy_2 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"admin", "policy"}, ""))
+
 	pattern_CerbosAdminService_EnablePolicy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"admin", "policy", "enable"}, ""))
 
 	pattern_CerbosAdminService_EnablePolicy_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"admin", "policy", "enable"}, ""))
@@ -2043,6 +2128,8 @@ var (
 	forward_CerbosAdminService_DisablePolicy_0 = runtime.ForwardResponseMessage
 
 	forward_CerbosAdminService_DisablePolicy_1 = runtime.ForwardResponseMessage
+
+	forward_CerbosAdminService_DisablePolicy_2 = runtime.ForwardResponseMessage
 
 	forward_CerbosAdminService_EnablePolicy_0 = runtime.ForwardResponseMessage
 
