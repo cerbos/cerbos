81d25193dbe9aba3bf44dab9bf94c88927d5d308
Charith Ellawala <charith@cerbos.dev>
2023-05-31 11:11:27 +0100

    enhancement(helm): Allow LoadBalancerIP to be set
    Also documents how to patch the chart with Kustomize.
    
    Fixes #1604
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- deploy/charts/validate.sh
+++ deploy/charts/validate.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CHART_DIR="${SCRIPT_DIR}/cerbos"
+MIN_KUBE_VERSION="1.23.0"
 
 if ! command -v kubeconform &> /dev/null; then
     go install github.com/yannh/kubeconform/cmd/kubeconform@latest
@@ -13,7 +14,8 @@ fi
 
 for VALUES_FILE in "${CHART_DIR}"/values*; do
     echo "Checking $VALUES_FILE"
-    helm template cerbos-test "$CHART_DIR" --values="$VALUES_FILE" | kubeconform \
+    helm template cerbos-test "$CHART_DIR" --values="$VALUES_FILE" --kube-version="$MIN_KUBE_VERSION" | kubeconform \
+        -kubernetes-version "$MIN_KUBE_VERSION" \
         -schema-location default \
         -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
         -summary
