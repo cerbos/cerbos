fab1fe1a9eaa62091c5ce3c89b539959a423ea17
Charith Ellawala <charith@cerbos.dev>
2023-06-21 16:29:49 +0100

    chore(test): Add E2E test for lenient scopes
    Run an E2E suite with lenient scopes enabled to ensure there are no
    regressions.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

---
+++ e2e/lenientscopes/lenientscopes_test.go
@@ -0,0 +1,16 @@
+// Copyright 2021-2023 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build e2e
+
+package lenientscopes_test
+
+import (
+	"testing"
+
+	"github.com/cerbos/cerbos/internal/test/e2e"
+)
+
+func TestLenientScopes(t *testing.T) {
+	e2e.RunSuites(t, e2e.WithContextID("lenientscopes"), e2e.WithImmutableStoreSuites())
+}
