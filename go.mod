9652c909f1e07e0d55e851fec800071ac0ddae57
Charith Ellawala <charithe@users.noreply.github.com>
2023-08-29 09:34:14 +0100

    feat: Audit log rotation support (#1766)
    Enables `file` audit logs to be automatically rotated.
    
    Also adds ability to write audit entries to multiple destinations.
    
    Fixes #1758
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- go.mod
+++ go.mod
@@ -98,6 +98,7 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.31.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v3 v3.0.1
 	helm.sh/helm/v3 v3.12.3
 	modernc.org/sqlite v1.25.0
--- internal/audit/file/conf.go
+++ internal/audit/file/conf.go
@@ -14,8 +14,22 @@ const confKey = audit.ConfKey + ".file"
 
 // Conf is optional configuration for file Audit.
 type Conf struct {
+	// LogRotation settings (optional).
+	LogRotation *LogRotationConf `yaml:"logRotation"`
 	// Path to the log file to use as output. The special values stdout and stderr can be used to write to stdout or stderr respectively.
-	Path string `yaml:"path" conf:",example=/path/to/file.log"`
+	Path string `yaml:"path" conf:"required,example=/path/to/file.log"`
+	// AdditionalPaths to mirror the log output. Has performance implications. Use with caution.
+	AdditionalPaths []string `yaml:"additionalPaths" conf:",example=[stdout]"`
+}
+
+//nolint:tagliatelle
+type LogRotationConf struct {
+	// MaxFileSizeMB sets the maximum size of individual log files in megabytes.
+	MaxFileSizeMB uint `yaml:"maxFileSizeMB" conf:",example=100"`
+	// MaxFileAgeDays sets the maximum age in days of old log files before they are deleted.
+	MaxFileAgeDays uint `yaml:"maxFileAgeDays" conf:",example=10"`
+	// MaxFileCount sets the maximum number of files to retain.
+	MaxFileCount uint `yaml:"maxFileCount" conf:",example=10"`
 }
 
 func (c *Conf) Key() string {
