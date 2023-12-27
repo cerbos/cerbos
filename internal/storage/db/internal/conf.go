dde81db82f71ba5464da35896e4843bbfbb89537
Charith Ellawala <charith@cerbos.dev>
2023-12-27 16:35:07 +0000

    enhancement: Configurable database connection retries
    Allow users to configure the retry settings for establishing a database
    connection. This is helpful in CI environments where the database
    service might not be available right away.
    
    Fixes #1919
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

---
+++ internal/storage/db/internal/conf.go
@@ -0,0 +1,82 @@
+// Copyright 2021-2023 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package internal
+
+import (
+	"errors"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	defaultRetryMaxAttempts = 3
+)
+
+// ConnPoolConf holds common SQL connection pool settings.
+type ConnPoolConf struct {
+	MaxLifetime time.Duration `yaml:"maxLifeTime"`
+	MaxIdleTime time.Duration `yaml:"maxIdleTime"`
+	MaxOpen     uint          `yaml:"maxOpen"`
+	MaxIdle     uint          `yaml:"maxIdle"`
+}
+
+func (cc *ConnPoolConf) Configure(db *sqlx.DB) {
+	if cc == nil {
+		return
+	}
+
+	db.SetConnMaxLifetime(cc.MaxLifetime)
+	db.SetConnMaxIdleTime(cc.MaxIdleTime)
+	db.SetMaxIdleConns(int(cc.MaxIdle))
+	db.SetMaxOpenConns(int(cc.MaxOpen))
+}
+
+// ConnRetryConf holds common retry settings for establishing a database connection.
+type ConnRetryConf struct {
+	// MaxAttempts is the maximum number of retries to attempt before giving up.
+	MaxAttempts uint64 `yaml:"maxAttempts"`
+	// InitialInterval is the initial wait period between retry attempts. Subsequent attempts will be longer depending on the attempt number.
+	InitialInterval time.Duration `yaml:"initialInterval"`
+	// MaxInterval is the maximum amount of time to wait between retry attempts.
+	MaxInterval time.Duration `yaml:"maxInterval"`
+}
+
+func (rc *ConnRetryConf) Validate() (outErr error) {
+	if rc == nil {
+		return nil
+	}
+
+	if rc.InitialInterval < 0 {
+		outErr = errors.Join(outErr, errors.New("retry.initialInterval must be a positive value"))
+	}
+
+	if rc.MaxInterval < 0 {
+		outErr = errors.Join(outErr, errors.New("retry.maxInterval must be a positive value"))
+	}
+
+	if rc.MaxInterval < rc.InitialInterval {
+		outErr = errors.Join(outErr, errors.New("retry.maxInterval must be larger than retry.initialInterval"))
+	}
+
+	return outErr
+}
+
+func (rc *ConnRetryConf) BackoffConf() backoff.BackOff {
+	if rc == nil {
+		return backoff.WithMaxRetries(backoff.NewExponentialBackOff(), defaultRetryMaxAttempts)
+	}
+
+	b := backoff.NewExponentialBackOff()
+	if rc.MaxInterval > 0 {
+		b.MaxInterval = rc.MaxInterval
+	}
+
+	if rc.InitialInterval > 0 {
+		b.InitialInterval = rc.InitialInterval
+	}
+
+	return backoff.WithMaxRetries(b, rc.MaxAttempts)
+}
--- internal/storage/db/postgres/conf.go
+++ internal/storage/db/postgres/conf.go
@@ -13,7 +13,8 @@ const confKey = storage.ConfKey + ".postgres"
 // Conf is required (if driver is set to 'postres') configuration for postres driver.
 // +desc=This section is required only if storage.driver is postgres.
 type Conf struct {
-	ConnPool *internal.ConnPoolConf `yaml:"connPool" conf:",example=\n  maxLifeTime: 60m\n  maxIdleTime: 45s\n  maxOpen: 4\n  maxIdle: 1"`
+	ConnPool  *internal.ConnPoolConf  `yaml:"connPool" conf:",example=\n  maxLifeTime: 60m\n  maxIdleTime: 45s\n  maxOpen: 4\n  maxIdle: 1"`
+	ConnRetry *internal.ConnRetryConf `yaml:"connRetry" conf:",example=\n  maxAttempts: 3\n  initialInterval: 0.5s\n  maxInterval: 60s"`
 	// URL is the Postgres connection URL. See https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 	URL string `yaml:"url" conf:"required,example=\"postgres://user:password@localhost:port/db\""`
 	// SkipSchemaCheck skips checking for required database tables on startup.
@@ -22,4 +23,8 @@ type Conf struct {
 
 func (c *Conf) Key() string {
 	return confKey
+}
+
+func (c *Conf) Validate() error {
+	return c.ConnRetry.Validate()
 }
