2773c3ae815871ae43d2f028f6c9490479198234
Andrew Haines <haines@cerbos.dev>
2024-03-06 15:33:52 +0000

    feat: Add `--client` flag to `cerbosctl version` (#2013)
    This PR adds a `--client` flag to `cerbosctl version`, so that it is
    possible to check the version of `cerbosctl` without specifying
    credentials or attempting to connect to a PDP server.
    
    It also adds missing help text to the `cerbos --version` flag, and fixes
    the top-level help of `cerbosctl` to include the information about
    providing credentials.
    
    ---------
    
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- cmd/cerbosctl/main.go
+++ cmd/cerbosctl/main.go
@@ -8,14 +8,39 @@ import (
 
 	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/client"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/root"
+	"github.com/cerbos/cerbos/internal/util"
 )
+
+const description = `A CLI for managing Cerbos
+
+The Cerbos Admin API must be enabled in order for these commands to work.
+The Admin API requires credentials. They can be provided using a netrc file,
+environment variables or command-line arguments.
+
+Environment variables
+
+	- CERBOS_SERVER: gRPC address of the Cerbos server
+	- CERBOS_USERNAME: Admin username
+	- CERBOS_PASSWORD: Admin password
+
+When more than one method is used to provide credentials, the precedence from lowest to
+highest is: netrc < environment < command line.
+
+Examples
+
+	# Connect to a TLS enabled server while skipping certificate verification and launch the decisions viewer
+	cerbosctl --server=localhost:3593 --username=user --password=password --insecure decisions
+
+	# Connect to a non-TLS server and launch the decisions viewer
+	cerbosctl --server=localhost:3593 --username=user --password=password --plaintext decisions`
 
 func main() {
 	cli := &root.Cli{}
 	ctx := kong.Parse(cli,
 		kong.Name("cerbosctl"),
-		kong.Description("A CLI for managing Cerbos"),
+		kong.Description(description),
 		kong.UsageOnError(),
+		kong.Vars{"version": util.AppVersion()},
 	)
 
 	c, err := client.GetClient(&cli.Globals)
--- cmd/cerbosctl/root/root.go
+++ cmd/cerbosctl/root/root.go
@@ -16,39 +16,15 @@ import (
 	"github.com/cerbos/cerbos/cmd/cerbosctl/version"
 )
 
-var help = `Cerbos instance administration commands
-The Cerbos Admin API must be enabled in order for these commands to work.
-The Admin API requires credentials. They can be provided using a netrc file, 
-environment variables or command-line arguments. 
-
-Environment variables
-
-CERBOS_SERVER: gRPC address of the Cerbos server
-CERBOS_USERNAME: Admin username
-CERBOS_PASSWORD: Admin password
-
-When more than one method is used to provide credentials, the precedence from lowest to 
-highest is: netrc < environment < command line.
-
-# Connect to a TLS enabled server while skipping certificate verification and launch the decisions viewer
-cerbosctl --server=localhost:3593 --username=user --password=password --insecure decisions
-
-# Connect to a non-TLS server and launch the decisions viewer
-cerbosctl --server=localhost:3593 --username=user --password=password --plaintext decisions`
-
 type Cli struct {
-	Version version.Cmd `cmd:"" help:"Show cerbosctl and PDP version"`
-	Get     get.Cmd     `cmd:"" help:"List or view policies and schemas"`
-	Store   store.Cmd   `cmd:"" help:"Store operations"`
+	Get get.Cmd `cmd:"" help:"List or view policies and schemas"`
 	flagset.Globals
+	Store     store.Cmd     `cmd:"" help:"Store operations"`
 	Delete    del.Cmd       `cmd:"" help:"Delete schemas"`
 	Disable   disable.Cmd   `cmd:"" help:"Disable policies"`
 	Enable    enable.Cmd    `cmd:"" help:"Enable policies"`
 	Put       put.Cmd       `cmd:"" help:"Put policies or schemas"`
 	Decisions decisions.Cmd `cmd:"" help:"Interactive decision log viewer"`
 	Audit     audit.Cmd     `cmd:"" help:"View audit logs"`
-}
-
-func (c *Cli) Help() string {
-	return help
+	Version   version.Cmd   `cmd:"" help:"Show cerbosctl and PDP version"`
 }
