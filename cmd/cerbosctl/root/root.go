5e9fdecfe76fee115a29d47c0b50369b0b5034a7
Andrew Haines <haines@cerbos.dev>
2024-03-06 15:09:42 +0000

    Fix top-level `cerbosctl` help
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- cmd/cerbosctl/root/root.go
+++ cmd/cerbosctl/root/root.go
@@ -16,26 +16,6 @@ import (
 	"github.com/cerbos/cerbos/cmd/cerbosctl/version"
 )
 
-var help = `Cerbos instance administration commands
-The Cerbos Admin API must be enabled in order for these commands to work.
-The Admin API requires credentials. They can be provided using a netrc file,
-environment variables or command-line arguments.
-
-Environment variables
-
-CERBOS_SERVER: gRPC address of the Cerbos server
-CERBOS_USERNAME: Admin username
-CERBOS_PASSWORD: Admin password
-
-When more than one method is used to provide credentials, the precedence from lowest to
-highest is: netrc < environment < command line.
-
-# Connect to a TLS enabled server while skipping certificate verification and launch the decisions viewer
-cerbosctl --server=localhost:3593 --username=user --password=password --insecure decisions
-
-# Connect to a non-TLS server and launch the decisions viewer
-cerbosctl --server=localhost:3593 --username=user --password=password --plaintext decisions`
-
 type Cli struct {
 	Get get.Cmd `cmd:"" help:"List or view policies and schemas"`
 	flagset.Globals
@@ -47,8 +27,4 @@ type Cli struct {
 	Decisions decisions.Cmd `cmd:"" help:"Interactive decision log viewer"`
 	Audit     audit.Cmd     `cmd:"" help:"View audit logs"`
 	Version   version.Cmd   `cmd:"" help:"Show cerbosctl and PDP version"`
-}
-
-func (c *Cli) Help() string {
-	return help
 }
