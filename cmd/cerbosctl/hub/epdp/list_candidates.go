fbc9e791835d84b667e67027531143f101531417
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2024-04-15 12:31:54 +0300

    Address some issues
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- cmd/cerbosctl/hub/epdp/list_candidates.go
+++ cmd/cerbosctl/hub/epdp/list_candidates.go
@@ -6,17 +6,17 @@ package epdp
 import (
 	"context"
 	"fmt"
+	"io/fs"
 	"os"
 
 	"github.com/alecthomas/kong"
 
 	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/printer"
 	"github.com/cerbos/cerbos/internal/namer"
-	"github.com/cerbos/cerbos/internal/policy"
 	"github.com/cerbos/cerbos/internal/storage/index"
 )
 
-const listCandidatesCmdHelp = `# List candidates
+const listCandidatesCmdHelp = `# List candidates for inclusion in embedded PDP
 
 cerbosctl hub epdp list-candidates ./path/to/repo
 
@@ -29,11 +29,9 @@ cerbosctl hub epdp list-candidates ./path/to/repo --no-headers
 cerbosctl hub epdp lc ./path/to/repo --no-headers`
 
 const (
-	embeddedPDPKey       = "hub.cerbos.cloud/embedded-pdp"
-	policyNotFoundInRepo = ""
+	embeddedPDPKey      = "hub.cerbos.cloud/embedded-pdp"
+	policyIDNotFoundYet = ""
 )
-
-type loadPolicyFn func(ctx context.Context, name string) (*policy.Wrapper, error)
 
 type ListCandidatesCmd struct {
 	Path      string `arg:"" type:"path" help:"Path to repository"`
@@ -41,29 +39,18 @@ type ListCandidatesCmd struct {
 }
 
 func (c *ListCandidatesCmd) Run(k *kong.Kong) error {
-	ctx := context.Background()
-	idx, err := index.Build(ctx, os.DirFS(c.Path))
-	if err != nil {
-		return fmt.Errorf("failed to build index: %w", err)
-	}
-
-	policyIDs, err := idx.ListPolicyIDs(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to list policies: %w", err)
-	}
-
 	tw := printer.NewTableWriter(k.Stdout)
 	if !c.NoHeaders {
 		tw.SetHeader([]string{"POLICY ID", "PATH"})
 	}
 
-	candidates, err := listCandidates(ctx, loadPolicy(idx), policyIDs...)
+	candidates, err := listCandidates(context.Background(), os.DirFS(c.Path))
 	if err != nil {
 		return fmt.Errorf("failed to list candidates: %w", err)
 	}
 
 	for policyKey, policyID := range candidates {
-		tw.Append([]string{namer.PolicyKeyFromFQN(policyKey), policyID})
+		tw.Append([]string{policyKey, policyID})
 	}
 	tw.Render()
 
@@ -74,35 +61,50 @@ func (c *ListCandidatesCmd) Help() string {
 	return listCandidatesCmdHelp
 }
 
-func listCandidates(ctx context.Context, loadPolicyFn loadPolicyFn, policyIDs ...string) (map[string]string, error) {
+func listCandidates(ctx context.Context, fsys fs.FS) (map[string]string, error) {
+	idx, err := index.Build(ctx, fsys)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build index: %w", err)
+	}
+
+	policyIDs, err := idx.ListPolicyIDs(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list policies: %w", err)
+	}
+
 	allPolicies := make(map[string]string, len(policyIDs))
 	annotatedPolicies := make(map[string]string, len(policyIDs))
 	for _, policyID := range policyIDs {
-		p, err := loadPolicyFn(ctx, policyID)
+		policies, err := idx.LoadPolicy(ctx, policyID)
 		if err != nil {
 			return nil, err
 		}
+		if len(policies) == 0 {
+			return nil, fmt.Errorf("failed to load policy %s", policyID)
+		}
 
-		policyKey := namer.PolicyKeyFromFQN(p.FQN)
+		wp := policies[0]
+		policyKey := namer.PolicyKeyFromFQN(wp.FQN)
 		allPolicies[policyKey] = policyID
-		if pID, ok := annotatedPolicies[policyKey]; ok && pID == policyNotFoundInRepo {
+		if pID, ok := annotatedPolicies[policyKey]; ok && pID == policyIDNotFoundYet {
 			annotatedPolicies[policyKey] = policyID
 		}
-		if p.Metadata == nil || p.Metadata.Annotations == nil {
+		if wp.Metadata == nil || wp.Metadata.Annotations == nil {
 			continue
 		}
 
 		//nolint:nestif
-		if value, ok := p.Metadata.Annotations[embeddedPDPKey]; ok && value == "true" {
+		if value, ok := wp.Metadata.Annotations[embeddedPDPKey]; ok && value == "true" {
 			annotatedPolicies[policyKey] = policyID
-			fqns := namer.FQNTree(p.Policy)
+			fqns := namer.FQNTree(wp.Policy)
 			if len(fqns) > 1 {
 				for _, fqn := range fqns[1:] {
-					if _, ok := annotatedPolicies[namer.PolicyKeyFromFQN(fqn)]; !ok {
-						if pID, ok := allPolicies[namer.PolicyKeyFromFQN(fqn)]; ok {
-							annotatedPolicies[namer.PolicyKeyFromFQN(fqn)] = pID
+					key := namer.PolicyKeyFromFQN(fqn)
+					if _, ok := annotatedPolicies[key]; !ok {
+						if pID, ok := allPolicies[key]; ok {
+							annotatedPolicies[key] = pID
 						} else {
-							annotatedPolicies[namer.PolicyKeyFromFQN(fqn)] = policyNotFoundInRepo
+							annotatedPolicies[key] = policyIDNotFoundYet
 						}
 					}
 				}
@@ -110,33 +112,9 @@ func listCandidates(ctx context.Context, loadPolicyFn loadPolicyFn, policyIDs ..
 		}
 	}
 
-	var policies map[string]string
 	if len(annotatedPolicies) != 0 {
-		policies = annotatedPolicies
-	} else {
-		policies = allPolicies
+		return annotatedPolicies, nil
 	}
 
-	for policyKey, policyID := range policies {
-		if policyID == policyNotFoundInRepo {
-			delete(policies, policyKey)
-		}
-	}
-
-	return policies, nil
-}
-
-func loadPolicy(idx index.Index) loadPolicyFn {
-	return func(ctx context.Context, name string) (*policy.Wrapper, error) {
-		policies, err := idx.LoadPolicy(ctx, name)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load policy %s: %w", name, err)
-		}
-
-		if len(policies) == 0 {
-			return nil, fmt.Errorf("failed to find any policy with the name %s", name)
-		}
-
-		return policies[0], nil
-	}
+	return allPolicies, nil
 }
--- cmd/cerbosctl/hub/epdp/list_candidates_test.go
+++ cmd/cerbosctl/hub/epdp/list_candidates_test.go
@@ -7,30 +7,22 @@ package epdp
 
 import (
 	"context"
-	"fmt"
-	"io/fs"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/cerbos/cerbos/internal/policy"
 	"github.com/cerbos/cerbos/internal/test"
 )
 
 func TestListCandidates(t *testing.T) {
 	testCases := []struct {
 		testFile     string
-		files        []string
+		expectedErr  string
 		expectedList []string
 	}{
 		{
 			testFile: "all_have_metadata.txt",
-			files: []string{
-				"purchase_order.yaml",
-				"regional/purchase_order.yaml",
-				"regional/uk/purchase_order.yaml",
-			},
 			expectedList: []string{
 				"resource.purchase_order.vdefault",
 				"resource.purchase_order.vdefault/regional",
@@ -38,42 +30,18 @@ func TestListCandidates(t *testing.T) {
 			},
 		},
 		{
-			testFile: "ancestor_not_in_repo.txt",
-			files: []string{
-				"regional/purchase_order.yaml",
-				"regional/uk/purchase_order.yaml",
-			},
-			expectedList: []string{
-				"resource.purchase_order.vdefault/regional",
-			},
+			testFile:    "ancestor_not_in_repo.txt",
+			expectedErr: "failed to build index: failed to build index: missing imports=0, missing scopes=1, duplicate definitions=0, load failures=0",
 		},
 		{
 			testFile: "ancestors.txt",
-			files: []string{
-				"purchase_order.yaml",
-				"regional/purchase_order.yaml",
-				"regional/uk/purchase_order.yaml",
-			},
 			expectedList: []string{
 				"resource.purchase_order.vdefault",
 				"resource.purchase_order.vdefault/regional",
-			},
-		},
-		{
-			testFile: "ancestors_not_in_repo.txt",
-			files: []string{
-				"regional/uk/purchase_order.yaml",
-			},
-			expectedList: []string{
-				"resource.purchase_order.vdefault/regional.uk",
 			},
 		},
 		{
 			testFile: "descendant_observed_first.txt",
-			files: []string{
-				"regional/purchase_order.yaml",
-				"purchase_order.yaml",
-			},
 			expectedList: []string{
 				"resource.purchase_order.vdefault",
 				"resource.purchase_order.vdefault/regional",
@@ -81,11 +49,6 @@ func TestListCandidates(t *testing.T) {
 		},
 		{
 			testFile: "irrelevant_metadata.txt",
-			files: []string{
-				"purchase_order.yaml",
-				"regional/purchase_order.yaml",
-				"regional/uk/purchase_order.yaml",
-			},
 			expectedList: []string{
 				"resource.purchase_order.vdefault",
 				"resource.purchase_order.vdefault/regional",
@@ -94,11 +57,6 @@ func TestListCandidates(t *testing.T) {
 		},
 		{
 			testFile: "no_metadata.txt",
-			files: []string{
-				"purchase_order.yaml",
-				"regional/purchase_order.yaml",
-				"regional/uk/purchase_order.yaml",
-			},
 			expectedList: []string{
 				"resource.purchase_order.vdefault",
 				"resource.purchase_order.vdefault/regional",
@@ -110,37 +68,21 @@ func TestListCandidates(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.testFile, func(t *testing.T) {
 			fsys := test.ExtractTxtArchiveToFS(t, filepath.Join("testdata", testCase.testFile))
-			pl := policyLoader{fsys: fsys}
-
-			candidates, err := listCandidates(context.Background(), pl.loadPolicy, testCase.files...)
-			require.NoError(t, err)
+			candidates, err := listCandidates(context.Background(), fsys)
+			if testCase.expectedErr != "" {
+				require.ErrorContains(t, err, "failed to build index: failed to build index: missing imports=0, missing scopes=1, duplicate definitions=0, load failures=0")
+			} else {
+				require.NoError(t, err)
+			}
+			require.Len(t, testCase.expectedList, len(candidates))
 
 			have := make([]string, 0, len(candidates))
-			for policyKey := range candidates {
+			for policyKey, policyID := range candidates {
+				require.NotEmpty(t, policyID, "policyID should not be empty")
 				have = append(have, policyKey)
 			}
 
-			require.Len(t, testCase.expectedList, len(candidates))
 			require.ElementsMatch(t, testCase.expectedList, have)
 		})
 	}
-}
-
-type policyLoader struct {
-	fsys fs.FS
-}
-
-func (pl policyLoader) loadPolicy(_ context.Context, name string) (*policy.Wrapper, error) {
-	f, err := pl.fsys.Open(name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open policy file: %w", err)
-	}
-
-	p, err := policy.ReadPolicy(f)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read policy: %w", err)
-	}
-
-	wp := policy.Wrap(p)
-	return &wp, nil
 }
