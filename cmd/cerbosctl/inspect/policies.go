40182111a3b4abe1b7748c6de10c555abbac3e0c
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2024-06-05 20:06:24 +0300

    Sort by store identifier
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- cmd/cerbosctl/inspect/policies.go
+++ cmd/cerbosctl/inspect/policies.go
@@ -12,6 +12,7 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/cerbos/cerbos-sdk-go/cerbos"
 
+	responsev1 "github.com/cerbos/cerbos/api/genpb/cerbos/response/v1"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/inspect/internal/flagset"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/client"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/printer"
@@ -63,14 +64,16 @@ func (c *PoliciesCmd) Run(k *kong.Kong, cctx *client.Context) error {
 		tw.SetHeader([]string{"POLICY ID", "ACTIONS", "VARIABLES"})
 	}
 
-	policyKeys := make([]string, 0, len(response.Results))
-	for policyKey := range response.Results {
-		policyKeys = append(policyKeys, policyKey)
+	results := make([]*responsev1.InspectPoliciesResponse_Result, 0, len(response.Results))
+	for _, result := range response.Results {
+		results = append(results, result)
 	}
-	sort.Strings(policyKeys)
 
-	for _, policyKey := range policyKeys {
-		result := response.Results[policyKey]
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].StoreIdentifier < results[j].StoreIdentifier
+	})
+
+	for _, result := range results {
 		variables := make([]string, len(result.Variables))
 		for idx, variable := range result.Variables {
 			variables[idx] = variable.Name
