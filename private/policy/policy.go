c1cec05d054343e169b92f117dd62b5659b76796
Charith Ellawala <charith@cerbos.dev>
2024-07-02 10:53:10 +0100

    chore: Add PolicyWrapper protobuf
    Represent the internal wrapper as a protobuf so that it can be ported
    across.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

---
+++ private/policy/policy.go
@@ -0,0 +1,25 @@
+// Copyright 2021-2024 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package policy
+
+import (
+	"io/fs"
+
+	policyv1 "github.com/cerbos/cerbos/api/genpb/cerbos/policy/v1"
+	sourcev1 "github.com/cerbos/cerbos/api/genpb/cerbos/source/v1"
+	"github.com/cerbos/cerbos/internal/namer"
+	"github.com/cerbos/cerbos/internal/policy"
+)
+
+func Wrap(p *policyv1.Policy) *sourcev1.PolicyWrapper {
+	return policy.Wrap(p).ToProto()
+}
+
+func ReadFromFile(fsys fs.FS, path string) (*policyv1.Policy, error) {
+	return policy.ReadPolicyFromFile(fsys, path)
+}
+
+func IDFromPolicyKey(key string) uint64 {
+	return namer.GenModuleIDFromFQN(namer.FQNFromPolicyKey(key)).RawValue()
+}
