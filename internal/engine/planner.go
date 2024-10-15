fd2bf695793816eca38f19e6fc1e5c9f58c4c3a6
Dennis Buduev <dbuduev@users.noreply.github.com>
2024-10-15 12:52:39 +1100

    WIP
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- internal/engine/planner.go
+++ internal/engine/planner.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"errors"
 
+	auditv1 "github.com/cerbos/cerbos/api/genpb/cerbos/audit/v1"
 	effectv1 "github.com/cerbos/cerbos/api/genpb/cerbos/effect/v1"
 	enginev1 "github.com/cerbos/cerbos/api/genpb/cerbos/engine/v1"
+	"github.com/cerbos/cerbos/internal/engine/tracer"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func PlannerEvaluateRolePolicy(ctx context.Context, evaluator Evaluator, input *enginev1.PlanResourcesInput) (effectv1.Effect, error) {
+func PlannerEvaluateRolePolicy(ctx context.Context, tctx tracer.Context, evaluator Evaluator, input *enginev1.PlanResourcesInput) (effectv1.Effect, *auditv1.AuditTrail, error) {
 	defaultEffect := effectv1.Effect_EFFECT_DENY
 	checkInput := enginev1.CheckInput{
 		RequestId: input.RequestId,
@@ -24,16 +26,17 @@ func PlannerEvaluateRolePolicy(ctx context.Context, evaluator Evaluator, input *
 		Actions:   []string{input.Action},
 		AuxData:   input.AuxData,
 	}
-	result, err := evaluator.Evaluate(ctx, nil, &checkInput)
+	result, err := evaluator.Evaluate(ctx, tctx, &checkInput)
 	if err != nil {
-		return defaultEffect, err
+		return defaultEffect, nil, err
 	}
 	if len(result.ValidationErrors) > 0 {
-		return defaultEffect, errors.New("role policies produced validation errors") // this shouldn't happen as role policies doesn't evaluate result.
+		return defaultEffect, nil, errors.New("role policies produced validation errors") // this shouldn't happen as role policies doesn't evaluate result.
 	}
 	eff, ok := result.Effects[input.Action]
 	if !ok {
-		return defaultEffect, errors.New("role policy evaluator unexpected result")
+		return defaultEffect, nil, errors.New("role policy evaluator unexpected result")
 	}
-	return eff.Effect, nil
+
+	return eff.Effect, result.AuditTrail, nil
 }
