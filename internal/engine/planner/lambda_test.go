88a1f1b65c61faeb296fe955b66be2beeab092bb
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-01-08 10:46:21 +1100

    lambda test
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

---
+++ internal/engine/planner/lambda_test.go
@@ -0,0 +1,17 @@
+// Copyright 2021-2025 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package planner
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWrapperDoNotPanic(t *testing.T) {
+	w := (*wrapper)(nil)
+	require.Nil(t, w.getArg(1).getArg(2).getListElement(3).e())
+	require.Zero(t, w.getArgsLen())
+	require.Empty(t, w.Function())
+}
