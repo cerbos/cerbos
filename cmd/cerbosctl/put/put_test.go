fb50db2931b46fe0000ad89f1885570e69f9acdd
Sam Lock <sam@swlock.co.uk>
2025-02-28 08:33:07 +0000

    add test for unordered scope permissions
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- cmd/cerbosctl/put/put_test.go
+++ cmd/cerbosctl/put/put_test.go
@@ -116,6 +116,7 @@ func testPutCmd(clientCtx *cmdclient.Context, globals *flagset.Globals) func(*te
 					"resource.leave_request.vdefault/acme.hr",
 					"resource.leave_request.vdefault/acme.hr.uk",
 					"resource.leave_request.vdefault/acme.hr.uk.brighton",
+					"resource.leave_request.vdefault/acme.hr.uk.brighton.kemptown",
 					"resource.leave_request.vdefault/acme.hr.uk.london",
 					"resource.leave_request.vstaging",
 					"resource.missing_attr.vdefault",
@@ -124,11 +125,13 @@ func testPutCmd(clientCtx *cmdclient.Context, globals *flagset.Globals) func(*te
 					"resource.purchase_order.vdefault",
 					"resource.runtime_effective_derived_roles.vdefault",
 					"resource.variables_referencing_variables.vdefault",
+					"role.accountant/acme",
 					"role.acme_assistant/acme.hr.uk",
 					"role.acme_creator/acme.hr.uk.brighton",
 					"role.acme_jr_admin/acme.hr.uk.brighton",
 					"role.acme_london_employee/acme.hr.uk.london",
 					"role.acme_travel_agent/acme.hr.uk",
+					"role.party_person/acme.hr.uk.brighton.kemptown",
 				}, listPolicies(t, clientCtx))
 			})
 
