c974c8db54436c6ab3d819538c50abf6744cbf19
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-04-17 16:32:42 +1000

    add for each action loop in EvaluateRuleTableQueryPlan
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- internal/engine/planner/merge.go
+++ internal/engine/planner/merge.go
@@ -11,40 +11,38 @@ import (
 	enginev1 "github.com/cerbos/cerbos/api/genpb/cerbos/engine/v1"
 )
 
-func MergeWithAnd(responses []*enginev1.PlanResourcesOutput) (*enginev1.PlanResourcesFilter, string, error) {
-	response := &enginev1.PlanResourcesFilter{
-		Kind: enginev1.PlanResourcesFilter_KIND_CONDITIONAL,
-	}
-	conds := make(map[string]*exprOp, len(responses))
-	for _, res := range responses {
-		switch res.Filter.Kind {
+func MergeWithAnd(filters []*enginev1.PlanResourcesFilter) (*enginev1.PlanResourcesFilter, string, error) {
+	conds := make(map[string]*exprOp, len(filters))
+	for _, f := range filters {
+		switch f.Kind {
 		case enginev1.PlanResourcesFilter_KIND_ALWAYS_ALLOWED:
 			continue
 		case enginev1.PlanResourcesFilter_KIND_ALWAYS_DENIED:
 			return &enginev1.PlanResourcesFilter{
 				Kind: enginev1.PlanResourcesFilter_KIND_ALWAYS_DENIED,
-			}, res.FilterDebug, nil
+			}, FilterToString(f), nil
 		case enginev1.PlanResourcesFilter_KIND_CONDITIONAL:
-			conds[res.FilterDebug] = res.Filter.Condition
+			conds[FilterToString(f)] = f.Condition
 		case enginev1.PlanResourcesFilter_KIND_UNSPECIFIED:
-			return nil, "", fmt.Errorf("unknown filter kind %s", res.Filter.Kind)
+			return nil, "", fmt.Errorf("unknown filter kind %s", f.Kind)
 		}
 	}
+	response := new(enginev1.PlanResourcesFilter)
 	switch len(conds) {
 	case 0:
-		response = &enginev1.PlanResourcesFilter{
-			Kind: enginev1.PlanResourcesFilter_KIND_ALWAYS_ALLOWED,
-		}
+		response.Kind = enginev1.PlanResourcesFilter_KIND_ALWAYS_ALLOWED, 
 	case 1:
 		for _, filter := range conds {
 			response.Condition = filter
 		}
+		response.Kind = enginev1.PlanResourcesFilter_KIND_CONDITIONAL
 	default:
-		filters := make([]*exprOp, 0, len(conds))
+		operands := make([]*exprOp, 0, len(conds))
 		for _, key := range slices.Sorted(maps.Keys(conds)) {
-			filters = append(filters, conds[key])
+			operands = append(operands, conds[key])
 		}
-		response.Condition = &exprOp{Node: mkExprOpExpr(And, filters...)}
+		response.Condition = &exprOp{Node: mkExprOpExpr(And, operands...)}
+		response.Kind = enginev1.PlanResourcesFilter_KIND_CONDITIONAL
 	}
 	return response, FilterToString(response), nil
 }
