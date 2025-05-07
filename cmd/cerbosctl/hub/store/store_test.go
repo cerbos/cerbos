12ed9bab540ddee04a1cbfe6a3310fb470e9ef6c
Charith Ellawala <charith@cerbos.dev>
2025-05-07 17:43:40 +0100

    Add testscript
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

---
+++ cmd/cerbosctl/hub/store/store_test.go
@@ -0,0 +1,60 @@
+// Copyright 2021-2025 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+package store_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+
+	"github.com/cerbos/cerbos/cmd/cerbosctl/root"
+)
+
+func TestStoreCmd(t *testing.T) {
+	// Create an .env file with the following values and run `just test ./cmd/cerbosctl/hub/store/...`
+	for _, envVar := range []string{"CERBOS_HUB_CLIENT_ID", "CERBOS_HUB_CLIENT_SECRET", "CERBOS_HUB_STORE_ID"} {
+		if os.Getenv(envVar) == "" {
+			t.Skipf("Skipping because %s is not set", envVar)
+		}
+	}
+
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata/testscripts",
+		Cmds: map[string]func(*testscript.TestScript, bool, []string){
+			"cerbosctl":         cerbosctl,
+			"cerbosctl_lenient": cerbosctlLenient,
+		},
+	})
+}
+
+func cerbosctl(ts *testscript.TestScript, neg bool, args []string) {
+	var exitCode int
+	exit := func(code int) {
+		exitCode = code
+	}
+	root.Run(args, exit, ts.Stdout(), ts.Stderr())
+
+	switch {
+	case neg && exitCode == 0:
+		ts.Fatalf("cerbosctl exited with %d", exitCode)
+	case !neg && exitCode > 0:
+		ts.Fatalf("cerbosctl exited with %d", exitCode)
+	}
+}
+
+func cerbosctlLenient(ts *testscript.TestScript, neg bool, args []string) {
+	var exitCode int
+	exit := func(code int) {
+		exitCode = code
+	}
+	root.Run(args, exit, ts.Stdout(), ts.Stderr())
+
+	switch {
+	case neg && (exitCode == 0 || exitCode == 5):
+		ts.Fatalf("cerbosctl exited with %d", exitCode)
+	case !neg && (exitCode != 0 && exitCode != 5):
+		ts.Fatalf("cerbosctl exited with %d", exitCode)
+	}
+}
--- cmd/cerbosctl/root/root.go
+++ cmd/cerbosctl/root/root.go
@@ -4,6 +4,10 @@
 package root
 
 import (
+	"fmt"
+	"io"
+
+	"github.com/alecthomas/kong"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/audit"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/decisions"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/del"
@@ -12,11 +16,36 @@ import (
 	"github.com/cerbos/cerbos/cmd/cerbosctl/get"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/hub"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/inspect"
+	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/client"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/internal/flagset"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/put"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/store"
 	"github.com/cerbos/cerbos/cmd/cerbosctl/version"
+	"github.com/cerbos/cerbos/internal/util"
 )
+
+const description = `A CLI for managing Cerbos
+
+The Cerbos Admin API must be enabled in order for these commands to work.
+The Admin API requires credentials. They can be provided using a netrc file,
+environment variables or command-line arguments.
+
+Environment variables
+
+	- CERBOS_SERVER: gRPC address of the Cerbos server
+	- CERBOS_USERNAME: Admin username
+	- CERBOS_PASSWORD: Admin password
+
+When more than one method is used to provide credentials, the precedence from lowest to
+highest is: netrc < environment < command line.
+
+Examples
+
+	# Connect to a TLS enabled server while skipping certificate verification and launch the decisions viewer
+	cerbosctl --server=localhost:3593 --username=user --password=password --insecure decisions
+
+	# Connect to a non-TLS server and launch the decisions viewer
+	cerbosctl --server=localhost:3593 --username=user --password=password --plaintext decisions`
 
 type Cli struct {
 	Get get.Cmd `cmd:"" help:"List or view policies and schemas"`
@@ -31,4 +60,50 @@ type Cli struct {
 	Decisions decisions.Cmd `cmd:"" help:"Interactive decision log viewer"`
 	Audit     audit.Cmd     `cmd:"" help:"View audit logs"`
 	Version   version.Cmd   `cmd:"" help:"Show cerbosctl and PDP version"`
+}
+
+func Run(args []string, exit func(int), stdout, stderr io.Writer) {
+	parser, cli := createParser(exit, stdout, stderr)
+
+	ctx, err := parser.Parse(args)
+	parser.FatalIfErrorf(err)
+
+	/*
+		clientCtx, err := createClientContext(cli)
+		ctx.FatalIfErrorf(err)
+	*/
+
+	ctx.FatalIfErrorf(ctx.Run(&cli.Globals))
+}
+
+func createParser(exit func(int), stdout, stderr io.Writer) (*kong.Kong, *Cli) {
+	cli := &Cli{}
+	parser, err := kong.New(cli,
+		kong.Name("cerbosctl"),
+		kong.Description(description),
+		kong.UsageOnError(),
+		kong.Vars{"version": util.AppVersion()},
+		kong.Exit(exit),
+		kong.Writers(stdout, stderr),
+		kong.BindToProvider(createClientContext),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return parser, cli
+}
+
+func createClientContext(globals *flagset.Globals) (*client.Context, error) {
+	c, err := client.GetClient(globals)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the client: %w", err)
+	}
+
+	ac, err := client.GetAdminClient(globals)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the admin client: %w", err)
+	}
+
+	return &client.Context{Client: c, AdminClient: ac}, nil
 }
