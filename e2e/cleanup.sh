d4e6db66ecf9777f173ee12a296e8e4cc10b33a1
Charith Ellawala <charithe@users.noreply.github.com>
2025-06-02 16:59:05 +0100

    chore(ci): Upgrade Helm and Helmfile (#2586)
    Helm secrets plugin doesn't work with Helm 3.18 but somehow the new
    version that's compatible is not getting installed automatically. It's
    about time we upgraded the deps anyway.
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>
    
    ---------
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- e2e/cleanup.sh
+++ e2e/cleanup.sh
@@ -4,10 +4,8 @@
 set -euo pipefail
 
 if [[ $# -ne 2 ]]; then
-  echo "Usage: $0 <context_id> <run_id>"
+    echo "Usage: $0 <context_id> <run_id>"
 fi
 
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-E2E_SRC_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)" E2E_CONTEXT_ID="$1" E2E_NS="e2e-$2" E2E_RUN_ID="$2" helmfile -f "${SCRIPT_DIR}/${1}/helmfile.yaml" destroy
-
+E2E_SRC_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)" E2E_CONTEXT_ID="$1" E2E_NS="e2e-$2" E2E_RUN_ID="$2" helmfile -f "${SCRIPT_DIR}/${1}/helmfile.yaml.gotmpl" destroy
