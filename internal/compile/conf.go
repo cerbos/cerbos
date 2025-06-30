84e942e801369e7f0c461d2778c3479ac0fd279a
Sam Lock <sam@swlock.co.uk>
2025-06-30 12:20:16 +0100

    only warn on deprecated compile conf fields
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- internal/compile/conf.go
+++ internal/compile/conf.go
@@ -4,10 +4,7 @@
 package compile
 
 import (
-	"errors"
 	"time"
-
-	"go.uber.org/multierr"
 
 	"github.com/cerbos/cerbos/internal/util"
 )
@@ -28,16 +25,14 @@ func (c *Conf) Key() string {
 
 func (c *Conf) SetDefaults() {}
 
-func (c *Conf) Validate() (outErr error) {
+func (c *Conf) Validate() error {
 	if c.CacheSize != 0 {
 		util.DeprecationWarning("compile.cacheSize")
-		outErr = multierr.Append(outErr, errors.New("compile.cacheSize section is no longer supported"))
 	}
 
 	if c.CacheDuration != 0 {
 		util.DeprecationWarning("compile.cacheDuration")
-		outErr = multierr.Append(outErr, errors.New("compile.cacheDuration section is no longer supported"))
 	}
 
-	return outErr
+	return nil
 }
