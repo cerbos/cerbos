7d18ea94a4cfb6ac38afc863a4975abfeb048427
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-01-23 20:20:31 +1100

    hide --run
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- cmd/cerbos/compile/compile.go
+++ cmd/cerbos/compile/compile.go
@@ -61,7 +61,7 @@ type Cmd struct { //betteralign:ignore
 	Dir           string                            `help:"Policy directory" arg:"" required:"" type:"path"`
 	IgnoreSchemas bool                              `help:"Ignore schemas during compilation"`
 	Tests         string                            `help:"[Deprecated] Path to the directory containing tests. Defaults to policy directory." type:"path"`
-	RunRegexp     string                            `help:"[Deprecated] Run only tests that match this regex" name:"run"`
+	RunRegexp     string                            `help:"[Deprecated] Run only tests that match this regex" name:"run" hidden:""`
 	TestFilter    flagset.TestFilter                `help:"Filter tests by dimensions (suite, test, principal, resource, action). Format: 'dimension=glob1,glob2;...'. Can be specified multiple times." name:"test-filter"`
 	SkipTests     bool                              `help:"Skip tests"`
 	SkipBatching  bool                              `help:"Skip batching tests"`
