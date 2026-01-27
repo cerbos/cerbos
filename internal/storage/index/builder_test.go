fba08e7fe505c39c49ef38761cec95ed332ff519
Sam Lock <sam@swlock.co.uk>
2026-01-27 11:26:18 +0000

    fix tests
    Signed-off-by: Sam Lock <sam@swlock.co.uk>

--- internal/storage/index/builder_test.go
+++ internal/storage/index/builder_test.go
@@ -44,7 +44,7 @@ func TestBuildIndexWithDisk(t *testing.T) {
 
 	t.Run("check_contents", func(t *testing.T) {
 		data := idxImpl.Inspect()
-		require.Len(t, data, 54)
+		require.Len(t, data, 57)
 
 		rp1 := filepath.Join("resource_policies", "policy_01.yaml")
 		rp2 := filepath.Join("resource_policies", "policy_02.yaml")
