4248605a7d52c99326f4384a21e8498953b02750
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-02-06 16:38:39 +1100

    change the hashing approach
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- internal/engine/tracer/sink_common.go
+++ internal/engine/tracer/sink_common.go
@@ -6,8 +6,9 @@ package tracer
 import (
 	"sync"
 
+	"github.com/cespare/xxhash/v2"
+
 	enginev1 "github.com/cerbos/cerbos/api/genpb/cerbos/engine/v1"
-	"github.com/cerbos/cerbos/internal/util"
 )
 
 type Sink interface {
@@ -40,10 +41,29 @@ func (c *Collector) Traces() []*enginev1.Trace {
 	return c.traces
 }
 
+const defaultCapacity = 256
+
 var (
-	defIndexPool = sync.Pool{New: func() any { return make(map[uint64]uint32, 256) }}
-	ptrHashPool  = sync.Pool{New: func() any { return make(map[*enginev1.Trace_Component]uint64, 256) }}
+	defIndexPool = sync.Pool{New: func() any { return make(map[uint64]uint32, defaultCapacity) }}
+	ptrHashPool  = sync.Pool{New: func() any { return make(map[*enginev1.Trace_Component]uint64, defaultCapacity) }}
+	serBufPool   = sync.Pool{New: func() any { b := make([]byte, 0, defaultCapacity); return &b }}
+	hasherPool   = sync.Pool{New: func() any { return xxhash.New() }}
 )
+
+func hashComponentVT(comp *enginev1.Trace_Component, hasher *xxhash.Digest, buf []byte) (uint64, []byte) {
+	size := comp.SizeVT()
+	if cap(buf) < size {
+		buf = make([]byte, size)
+	} else {
+		buf = buf[:size]
+	}
+
+	_, _ = comp.MarshalToSizedBufferVT(buf)
+	hasher.Reset()
+	_, _ = hasher.Write(buf)
+
+	return hasher.Sum64(), buf
+}
 
 func TracesToBatch(traces []*enginev1.Trace) *enginev1.TraceBatch {
 	if len(traces) == 0 {
@@ -55,10 +75,13 @@ func TracesToBatch(traces []*enginev1.Trace) *enginev1.TraceBatch {
 		totalComponents += len(trace.Components)
 	}
 
-	defs := make([]*enginev1.Trace_Component, 0, 256)
+	defs := make([]*enginev1.Trace_Component, 0, defaultCapacity)
 
-	defIndex := defIndexPool.Get().(map[uint64]uint32)
-	ptrToHash := ptrHashPool.Get().(map[*enginev1.Trace_Component]uint64)
+	defIndex := defIndexPool.Get().(map[uint64]uint32)       //nolint:forcetypeassert
+	ptrToHash := ptrHashPool.Get().(map[*enginev1.Trace_Component]uint64) //nolint:forcetypeassert
+	serBufPtr := serBufPool.Get().(*[]byte)                  //nolint:forcetypeassert
+	serBuf := *serBufPtr
+	hasher := hasherPool.Get().(*xxhash.Digest) //nolint:forcetypeassert
 
 	entryBuf := make([]enginev1.TraceEntry, len(traces))
 	entries := make([]*enginev1.TraceEntry, len(traces))
@@ -71,7 +94,7 @@ func TracesToBatch(traces []*enginev1.Trace) *enginev1.TraceBatch {
 		for j, comp := range trace.Components {
 			key, ok := ptrToHash[comp]
 			if !ok {
-				key = util.HashPB(comp, nil)
+				key, serBuf = hashComponentVT(comp, hasher, serBuf)
 				ptrToHash[comp] = key
 			}
 			if idx, ok := defIndex[key]; ok {
@@ -92,6 +115,9 @@ func TracesToBatch(traces []*enginev1.Trace) *enginev1.TraceBatch {
 	clear(ptrToHash)
 	defIndexPool.Put(defIndex)
 	ptrHashPool.Put(ptrToHash)
+	*serBufPtr = serBuf[:0]
+	serBufPool.Put(serBufPtr)
+	hasherPool.Put(hasher)
 
 	return &enginev1.TraceBatch{
 		Definitions: defs,
